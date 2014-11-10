210b5521921cc45803b75d719c523e19e1d929bc
hursevich <hursevich@gmail.com>
2014-11-11 00:36:48 +0300

    - job type fixes

--- sources/zafira-ws/src/main/java/com/qaprosoft/zafira/ws/dto/JobType.java
+++ sources/zafira-ws/src/main/java/com/qaprosoft/zafira/ws/dto/JobType.java
@@ -4,16 +4,53 @@ import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.qaprosoft.zafira.dbaccess.model.Job;
 
 @JsonInclude(Include.NON_NULL)
-public class JobType extends Job
+public class JobType extends AbstractType
 {
-	private static final long serialVersionUID = -4887504085866015187L;
-	
+	private Long userId;
+	private String name;
 	@NotNull
+	private String jobURL;
+	private String jenkinsHost;
+
+	public Long getUserId()
+	{
+		return userId;
+	}
+
+	public void setUserId(Long userId)
+	{
+		this.userId = userId;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public String getJobURL()
+	{
+		return jobURL;
+	}
+
 	public void setJobURL(String jobURL)
 	{
-		super.setJobURL(jobURL);
+		this.jobURL = jobURL;
+	}
+
+	public String getJenkinsHost()
+	{
+		return jenkinsHost;
+	}
+
+	public void setJenkinsHost(String jenkinsHost)
+	{
+		this.jenkinsHost = jenkinsHost;
 	}
 }
