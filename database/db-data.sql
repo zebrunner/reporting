55f3c68022e4600d5890ab2a1281db0a221985a6
vdelendik <vadim.delendik@gmail.com>
2016-10-10 21:26:46 +0300

    improved performance widget query and model

--- database/db-data.sql
+++ database/db-data.sql
@@ -7,11 +7,19 @@ BEGIN
   INSERT INTO zafira.DASHBOARDS (TITLE, TYPE) VALUES ('Performance dashboard', 'PERFORMANCE') RETURNING id INTO dashboard_id;
 
   INSERT INTO zafira.WIDGETS (TITLE, TYPE, SQL, MODEL) VALUES ('Performance widget', 'linechart',
-	'select tm.operation, tm.elapsed AS "ELAPSED", tc.env, tm.created_at AS "CREATED_AT" from zafira.test_metrics tm inner join zafira.tests t on tm.test_id = t.id
-		inner join zafira.test_configs tc on tc.id = t.test_config_id where t.test_case_id = #{test_case_id} #{time_step} order by tm.operation asc, tc.env asc, tm.created_at asc',
+	'set schema 'zafira';
+SELECT TEST_CONFIGS.ENV || '-' || TEST_CONFIGS.DEVICE AS env, 
+	TEST_METRICS.OPERATION, 
+	TEST_METRICS.ELAPSED AS "ELAPSED",
+	TEST_METRICS.CREATED_AT AS "CREATED_AT"
+FROM TEST_METRICS INNER JOIN 
+	TESTS ON TEST_METRICS.TEST_ID = TESTS.ID INNER JOIN
+	TEST_CONFIGS ON TEST_CONFIGS.ID = TESTS.TEST_CONFIG_ID
+WHERE TESTS.TEST_CASE_ID = #{test_case_id} #{time_step}
+ORDER BY env, "CREATED_AT"',
     '{"series":[{"axis":"y","dataset":"dataset","key":"ELAPSED","label":"ELAPSED","color":"#5cb85c","thickness":"10px",
-    	"type":["line","dot","area"],"id":"ELAPSED"}],"axes":{"x":{"key":"CREATED_AT","type":"date","ticks": "functions(value) {return \"wow!\"}"}}}')
+    	"type":["line","dot"],"id":"ELAPSED"}],"axes":{"x":{"key":"CREATED_AT","type":"int","ticks": "functions(value) {return \"wow!\"}"}}}')
     RETURNING id INTO widget_id;
 
-	INSERT INTO zafira.DASHBOARDS_WIDGETS (DASHBOARD_ID, WIDGET_ID, POSITION, SIZE) VALUES (dashboard_id, widget_id, 1, 4);
+	INSERT INTO zafira.DASHBOARDS_WIDGETS (DASHBOARD_ID, WIDGET_ID, POSITION, SIZE) VALUES (dashboard_id, widget_id, 1, 12);
 END$$;
