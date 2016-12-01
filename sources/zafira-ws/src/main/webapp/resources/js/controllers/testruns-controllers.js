c1078032d3cc25e26e69c6d75d0183ac28f2c99f
Aliaksei Khursevich <hursrevich@gmail.com>
2016-12-01 16:31:13 +0300

    - fixed https://github.com/qaprosoft/zafira/issues/4

--- sources/zafira-ws/src/main/webapp/resources/js/controllers/testruns-controllers.js
+++ sources/zafira-ws/src/main/webapp/resources/js/controllers/testruns-controllers.js
@@ -181,8 +181,12 @@ ZafiraApp.controller('TestRunsListCtrl', [ '$scope', '$rootScope', '$http' ,'$lo
 		{
 			$scope.testRunSearchCriteria.id = $scope.testRunId;
 		}
+		else
+		{
+			$scope.testRunSearchCriteria = ProjectProvider.initProject($scope.testRunSearchCriteria);
+		}
 		
-		$http.post('tests/runs/search', ProjectProvider.initProject($scope.testRunSearchCriteria)).success(function(data) {
+		$http.post('tests/runs/search', $scope.testRunSearchCriteria).success(function(data) {
 			
 			$scope.testRuns = {};
 			
