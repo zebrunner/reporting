7dac0eb9057201c978fb4d3f23cfca3c9a66f5e4
Aliaksei Khursevich <hursrevich@gmail.com>
2017-03-02 02:16:19 +0300

    - fixed https://github.com/qaprosoft/zafira/issues/47

--- sources/zafira-services/src/main/java/com/qaprosoft/zafira/services/services/JenkinsService.java
+++ sources/zafira-services/src/main/java/com/qaprosoft/zafira/services/services/JenkinsService.java
@@ -8,14 +8,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import com.google.common.base.Optional;
 import com.offbytwo.jenkins.JenkinsServer;
+import com.offbytwo.jenkins.model.FolderJob;
+import com.offbytwo.jenkins.model.JobWithDetails;
 import com.offbytwo.jenkins.model.QueueReference;
+import com.qaprosoft.zafira.models.db.Job;
 
 @Service
 public class JenkinsService
 {
 	private static Logger LOGGER = LoggerFactory.getLogger(JenkinsService.class);
 
+	private final String FOLDER_REGEX = ".+job\\/.+\\/job.+";
+	
 	private JenkinsServer server;
 
 	public JenkinsService(String url, String username, String password)
@@ -47,17 +53,29 @@ public class JenkinsService
 		return running;
 	}
 	
-	public boolean rerunJob(String jobName, Integer buildNumber, boolean rerunFailures)
+	public boolean rerunJob(Job ciJob, Integer buildNumber, boolean rerunFailures)
 	{
 		boolean success = false;
 		try
 		{
-			Map<String, String> params = server.getJob(jobName).getBuildByNumber(buildNumber).details().getParameters();
+			JobWithDetails job = null;
+			if(ciJob.getJobURL().matches(FOLDER_REGEX))
+			{
+				String folderName = ciJob.getJobURL().split("/job/")[1];
+				Optional<FolderJob> folder = server.getFolderJob(server.getJob(folderName));
+				job = server.getJob(folder.get(), ciJob.getName());
+			}
+			else
+			{
+				job = server.getJob(ciJob.getName());
+			}
+			
+			Map<String, String> params = job.getBuildByNumber(buildNumber).details().getParameters();
 			if(rerunFailures)
 			{
 				params.put("rerun_failures", "true");
 			}
-			QueueReference reference = server.getJob(jobName).build(params, true);
+			QueueReference reference = job.build(params, true);
 			success = reference != null && !StringUtils.isEmpty(reference.getQueueItemUrlPart());
 		}
 		catch (Exception e)
