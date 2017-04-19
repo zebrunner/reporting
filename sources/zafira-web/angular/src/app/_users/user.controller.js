f292127229817ad6862b6566ead68090da9aba10
brutskov <brutskov@onflydev.com>
2017-04-19 19:23:59 +0300

    added user_profile_modal dialog actions

--- sources/zafira-web/angular/src/app/_users/user.controller.js
+++ sources/zafira-web/angular/src/app/_users/user.controller.js
@@ -1,19 +1,19 @@
 (function () {
     'use strict';
- 
+
     angular
         .module('app.auth')
         .controller('UserProfileController', ['$scope', '$location', 'UserService', 'UtilService', UserProfileController])
-        .controller('UserListController', ['$scope', '$location', 'UserService', 'UtilService', UserListController])
- 
+        .controller('UserListController', ['$scope', '$location', '$mdDialog', 'UserService', 'UtilService', UserListController])
+
     // **************************************************************************
     function UserProfileController($scope, $location, UserService, UtilService) {
- 
+
     	$scope.UtilService = UtilService;
-    	
+
     	$scope.user = {};
     	$scope.changePassword = {};
- 
+
         (function initController() {
         	UserService.getUserProfile()
         	.then(function (rs) {
@@ -27,7 +27,7 @@
         		}
             });
         })();
-        
+
         $scope.updateUserProfile = function(profile)
         {
         	UserService.updateUserProfile(profile)
@@ -43,7 +43,7 @@
         		}
             });
         };
-        
+
         $scope.updateUserPassword = function(changePassword)
         {
         	UserService.updateUserPassword(changePassword)
@@ -62,11 +62,12 @@
     }
 
     // **************************************************************************
-    function UserListController($scope, $location, UserService, UtilService) {
+    function UserListController($scope, $location, $mdDialog, UserService, UtilService) {
 
     	$scope.sc = {page : 1, pageSize : 20};
     	$scope.users = [];
-    	
+        $scope.order = 'username';
+
     	$scope.openPage = function (page) {
 			$scope.sc.page = page;
 			UserService.searchUsers($scope.sc).then(function(rs) {
@@ -80,7 +81,26 @@
         		}
 			});
         };
-    	
+
+        $scope.showEditProfileDialog = function(event, user) {
+            $mdDialog.show({
+                controller: function ($scope, $mdDialog) {
+                    $scope.user = user;
+                    $scope.cancel = function() {
+                        $mdDialog.cancel();
+                    };
+                },
+                templateUrl: 'app/_users/user_profile_modal.html',
+                parent: angular.element(document.body),
+                targetEvent: event,
+                clickOutsideToClose:true,
+                fullscreen: true
+            })
+                .then(function(answer) {
+                }, function() {
+                });
+        };
+
 		(function initController() {
 			$scope.openPage(1);
 		})();
