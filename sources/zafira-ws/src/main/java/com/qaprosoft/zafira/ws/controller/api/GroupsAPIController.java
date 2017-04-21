2d0c556ca6ca7d19dcdf7182fba792b9962b5104
brutskov <brutskov@onflydev.com>
2017-04-21 16:37:38 +0300

    added groups

---
+++ sources/zafira-ws/src/main/java/com/qaprosoft/zafira/ws/controller/api/GroupsAPIController.java
@@ -0,0 +1,93 @@
+package com.qaprosoft.zafira.ws.controller.api;
+
+import com.qaprosoft.zafira.models.db.Group;
+import com.qaprosoft.zafira.models.dto.user.UserType;
+import com.qaprosoft.zafira.services.exceptions.ServiceException;
+import com.qaprosoft.zafira.services.services.GroupService;
+import com.qaprosoft.zafira.ws.swagger.annotations.ResponseStatusDetails;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+@Api(value = "Groups API")
+@CrossOrigin
+@RequestMapping("api/groups")
+public class GroupsAPIController {
+
+    @Autowired
+    private GroupService groupService;
+
+    @ResponseStatusDetails
+    @ApiOperation(value = "Create group", nickname = "createGroup", code = 200, httpMethod = "POST", response = Group.class)
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    Group createGroup(@RequestBody Group group) throws ServiceException
+    {
+        return groupService.createGroup(group);
+    }
+
+    @ResponseStatusDetails
+    @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value = "Get group", nickname = "getGroup", code = 200, httpMethod = "GET", response = Group.class)
+    @RequestMapping(value = "{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody Group getGroup(@PathVariable(value = "id") long id) throws ServiceException
+    {
+        return groupService.getGroupById(id);
+    }
+
+    @ResponseStatusDetails
+    @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value = "Get all groups", nickname = "getAllGroups", code = 200, httpMethod = "GET", response = List.class)
+    @RequestMapping(value = "all", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    List<Group> getAllGroups() throws ServiceException
+    {
+        return groupService.getAllGroups();
+    }
+
+    @ResponseStatusDetails
+    @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value = "Get groups count", nickname = "getGroupsCount", code = 200, httpMethod = "GET", response = Integer.class)
+    @RequestMapping(value = "count", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody Integer getGroupsCount() throws ServiceException
+    {
+        return groupService.getGroupsCount();
+    }
+
+    @ResponseStatusDetails
+    @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value = "Get roles", nickname = "getRoles", code = 200, httpMethod = "GET", response = List.class)
+    @RequestMapping(value = "roles", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody List<Group.Role> getRoles() throws ServiceException
+    {
+        return Arrays.asList(Group.Role.values());
+    }
+
+    @ResponseStatusDetails
+    @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value = "Update group", nickname = "updateGroup", code = 200, httpMethod = "PUT", response = Group.class)
+    @RequestMapping(method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody Group updateGroup(@RequestBody Group group) throws ServiceException
+    {
+        return groupService.updateGroup(group);
+    }
+
+    @ResponseStatusDetails
+    @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value = "Delete group", nickname = "deleteGroup", code = 200, httpMethod = "DELETE")
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void deleteGroup(@PathVariable(value = "id") long id) throws ServiceException
+    {
+        groupService.deleteGroup(id);
+    }
+}
