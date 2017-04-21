9a93a7b3a4785b7b534d424f8d9f77c1abe805bc
brutskov <brutskov@onflydev.com>
2017-04-21 17:12:15 +0300

    set GroupController

--- sources/zafira-web/angular/src/app/_users/user.controller.js
+++ sources/zafira-web/angular/src/app/_users/user.controller.js
@@ -217,164 +217,7 @@
 
         $scope.showGroupDialog = function(event) {
             $mdDialog.show({
-                controller: function ($scope, $mdDialog) {
-                    $scope.group = {};
-                    $scope.groups = [];
-                    $scope.roles = [];
-                    $scope.group.users = [];
-                    $scope.showGroups = false;
-                    $scope.getRoles = function() {
-                        GroupService.getRoles().then(function(rs) {
-                            if(rs.success)
-                            {
-                                $scope.roles = rs.data;
-                            }
-                            else
-                            {
-                                alertify.error(rs.message);
-                            }
-                        });
-                    };
-                    $scope.getGroupsCount = function() {
-                        GroupService.getGroupsCount().then(function(rs) {
-                            if(rs.success)
-                            {
-                                $scope.count = rs.data;
-                            }
-                            else
-                            {
-                                alertify.error(rs.message);
-                            }
-                        });
-                    };
-                    $scope.getAllGroups = function() {
-                        GroupService.getAllGroups().then(function(rs) {
-                            if(rs.success)
-                            {
-                                $scope.groups = rs.data;
-                            }
-                            else
-                            {
-                                alertify.error(rs.message);
-                            }
-                        });
-                    };
-                    $scope.createGroup = function(group) {
-                        GroupService.createGroup(group).then(function(rs) {
-                            if(rs.success)
-                            {
-                                $scope.getAllGroups();
-                                alertify.success('Group "' + group.name + '" was created');
-                                $scope.count ++;
-                            }
-                            else
-                            {
-                                alertify.error(rs.message);
-                            }
-                        });
-                    };
-                    $scope.getGroup = function(id) {
-                        GroupService.getGroup(id).then(function(rs) {
-                            if(rs.success)
-                            {
-                                $scope.group = rs.data;
-                            }
-                            else
-                            {
-                                alertify.error(rs.message);
-                            }
-                        });
-                    };
-                    $scope.updateGroup = function(group) {
-                        GroupService.updateGroup(group).then(function(rs) {
-                            if(rs.success)
-                            {
-                            }
-                            else
-                            {
-                                alertify.error(rs.message);
-                            }
-                        });
-                    };
-                    $scope.deleteGroup = function(group) {
-                        GroupService.deleteGroup(group.id).then(function(rs) {
-                            if(rs.success)
-                            {
-                                $scope.getAllGroups();
-                                $scope.count --;
-                                alertify.success('Group "' + group.name + '" was deleted');
-                            }
-                            else
-                            {
-                                alertify.error(rs.message);
-                            }
-                        });
-                    };
-                    $scope.addUserToGroup = function(user, group) {
-                        UserService.addUserToGroup(user, group.id).then(function(rs) {
-                            if(rs.success)
-                            {
-                                alertify.success('User "' + user.username + '" was added to group "' + group.name + '"');
-                            }
-                            else
-                            {
-                                alertify.error(rs.message);
-                            }
-                        });
-                    };
-                    $scope.deleteUserFromGroup = function(user, group) {
-                        UserService.deleteUserFromGroup(user.id, group.id).then(function(rs) {
-                            if(rs.success)
-                            {
-                                alertify.success('User "' + user.username + '" was deleted from group "' + group.name + '"');
-                            }
-                            else
-                            {
-                                alertify.error(rs.message);
-                            }
-                        });
-                    };
-
-                    $scope.usersSearchCriteria = {};
-                    $scope.querySearch = querySearch;
-                    function querySearch (criteria, group) {
-                        $scope.usersSearchCriteria.username = criteria;
-                        return UserService.searchUsersWithQuery($scope.usersSearchCriteria).then(function(rs) {
-                            if(rs.success)
-                            {
-                                console.log(Array.isArray(rs.data.results));
-                                return rs.data.results.filter(searchFilter(group));
-                            }
-                            else
-                            {
-                                alertify.error(rs.message);
-                            }
-                        }).finally(function (rs) {
-                        });
-                    }
-                    function searchFilter(group) {
-                        return function filterFn(user) {
-                            var users = group.users;
-                            for(var i = 0; i < users.length; i++) {
-                                if(users[i].id == user.id) {
-                                    return false;
-                                }
-                            }
-                            return true;
-                        };
-                    }
-                    $scope.hide = function() {
-                        $mdDialog.hide();
-                    };
-                    $scope.cancel = function() {
-                        $mdDialog.cancel();
-                    };
-                    (function initController() {
-                        $scope.getRoles();
-                        $scope.getGroupsCount();
-                        $scope.getAllGroups();
-                    })();
-                },
+                controller: GroupController,
                 templateUrl: 'app/_users/group_modal.html',
                 parent: angular.element(document.body),
                 targetEvent: event,
@@ -390,4 +233,165 @@
 			$scope.search(1);
 		})();
 	}
+
+    // **************************************************************************
+    function GroupController($scope, $mdDialog, UserService, GroupService, UtilService) {
+        $scope.UtilService = UtilService;
+        $scope.group = {};
+        $scope.groups = [];
+        $scope.roles = [];
+        $scope.group.users = [];
+        $scope.showGroups = false;
+        $scope.getRoles = function() {
+            GroupService.getRoles().then(function(rs) {
+                if(rs.success)
+                {
+                    $scope.roles = rs.data;
+                }
+                else
+                {
+                    alertify.error(rs.message);
+                }
+            });
+        };
+        $scope.getGroupsCount = function() {
+            GroupService.getGroupsCount().then(function(rs) {
+                if(rs.success)
+                {
+                    $scope.count = rs.data;
+                }
+                else
+                {
+                    alertify.error(rs.message);
+                }
+            });
+        };
+        $scope.getAllGroups = function() {
+            GroupService.getAllGroups().then(function(rs) {
+                if(rs.success)
+                {
+                    $scope.groups = rs.data;
+                }
+                else
+                {
+                    alertify.error(rs.message);
+                }
+            });
+        };
+        $scope.createGroup = function(group) {
+            GroupService.createGroup(group).then(function(rs) {
+                if(rs.success)
+                {
+                    $scope.getAllGroups();
+                    alertify.success('Group "' + group.name + '" was created');
+                    $scope.count ++;
+                }
+                else
+                {
+                    alertify.error(rs.message);
+                }
+            });
+        };
+        $scope.getGroup = function(id) {
+            GroupService.getGroup(id).then(function(rs) {
+                if(rs.success)
+                {
+                    $scope.group = rs.data;
+                }
+                else
+                {
+                    alertify.error(rs.message);
+                }
+            });
+        };
+        $scope.updateGroup = function(group) {
+            GroupService.updateGroup(group).then(function(rs) {
+                if(rs.success)
+                {
+                }
+                else
+                {
+                    alertify.error(rs.message);
+                }
+            });
+        };
+        $scope.deleteGroup = function(group) {
+            GroupService.deleteGroup(group.id).then(function(rs) {
+                if(rs.success)
+                {
+                    $scope.getAllGroups();
+                    $scope.count --;
+                    alertify.success('Group "' + group.name + '" was deleted');
+                }
+                else
+                {
+                    alertify.error(rs.message);
+                }
+            });
+        };
+        $scope.addUserToGroup = function(user, group) {
+            UserService.addUserToGroup(user, group.id).then(function(rs) {
+                if(rs.success)
+                {
+                    alertify.success('User "' + user.username + '" was added to group "' + group.name + '"');
+                }
+                else
+                {
+                    alertify.error(rs.message);
+                }
+            });
+        };
+        $scope.deleteUserFromGroup = function(user, group) {
+            UserService.deleteUserFromGroup(user.id, group.id).then(function(rs) {
+                if(rs.success)
+                {
+                    alertify.success('User "' + user.username + '" was deleted from group "' + group.name + '"');
+                }
+                else
+                {
+                    alertify.error(rs.message);
+                }
+            });
+        };
+
+        $scope.usersSearchCriteria = {};
+        $scope.querySearch = querySearch;
+        function querySearch (criteria, group) {
+            $scope.usersSearchCriteria.username = criteria;
+            return UserService.searchUsersWithQuery($scope.usersSearchCriteria).then(function(rs) {
+                if(rs.success)
+                {
+                    console.log(Array.isArray(rs.data.results));
+                    return rs.data.results.filter(searchFilter(group));
+                }
+                else
+                {
+                    alertify.error(rs.message);
+                }
+            }).finally(function (rs) {
+            });
+        }
+        function searchFilter(group) {
+            return function filterFn(user) {
+                var users = group.users;
+                for(var i = 0; i < users.length; i++) {
+                    if(users[i].id == user.id) {
+                        return false;
+                    }
+                }
+                return true;
+            };
+        }
+        $scope.hide = function() {
+            $mdDialog.hide();
+        };
+        $scope.cancel = function() {
+            $mdDialog.cancel();
+        };
+        (function initController() {
+            $scope.getRoles();
+            $scope.getGroupsCount();
+            $scope.getAllGroups();
+        })();
+    }
 })();
