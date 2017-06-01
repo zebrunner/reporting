0a3e208c5bb6cf20acba99901c05f350557ed685
irina <tsvrko@gmail.com>
2017-06-01 19:36:44 +0300

    demo link fixed

--- sources/zafira-services/src/main/java/com/qaprosoft/zafira/services/services/TestRunService.java
+++ sources/zafira-services/src/main/java/com/qaprosoft/zafira/services/services/TestRunService.java
@@ -193,6 +193,10 @@ public class TestRunService
 					{
 						testRun.setAppVersion(arg.getValue());
 					}
+					else if("keep_all_screenshots".equals(arg.getKey()))
+					{
+						testRun.setScreenshots(Boolean.valueOf(arg.getValue()));
+					}
 				}
 			}
 		}
@@ -364,6 +368,7 @@ public class TestRunService
 		configuration.getArg().add(new Argument("zafira_service_url", wsURL));
 
 		List<Test> tests = testService.getTestsByTestRunId(testRunId);
+
 		if (testRun.getPlatform().equals("API")){
 			for (Test test:tests) {
 				test.setDemoURL(null);
