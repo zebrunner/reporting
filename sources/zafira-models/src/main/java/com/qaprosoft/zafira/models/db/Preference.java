39405f54046274978c04983b18c124d6f0b266ed
tsvrko <tsvrko@gmail.com>
2017-09-19 19:57:19 +0300

    user preferences initial commit

---
+++ sources/zafira-models/src/main/java/com/qaprosoft/zafira/models/db/Preference.java
@@ -0,0 +1,9 @@
+package com.qaprosoft.zafira.models.db;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Preference extends AbstractEntity{
+
+    private static final long serialVersionUID = 3544699350398796894L;
+}
