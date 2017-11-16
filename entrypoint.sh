1e2601293857eab4af2a6541c8f4f08f8abb3007
akhursevich <hursrevich@gmail.com>
2017-11-16 17:20:33 +0300

    https://github.com/qaprosoft/zafira/issues/365 fixed

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,19 @@
 
 # Change Zafira API URL
 cd $CATALINA_HOME/temp
+
 unzip -qq zafira.war -d zafira
 rm -rf zafira.war
 sed -i -e 's#http://localhost:8080#'"$ZAFIRA_URL"'#g' zafira/scripts/app.js
 cd zafira && zip -r ../zafira.war * && cd ..
+
+if [ "$ZAFIRA_LDAP_ENABLED" == "true" ];
+then
+  unzip -qq zafira-ws.war -d zafira-ws
+  rm -rf zafira-ws.war
+  sed -i -e 's#<!-- security:authentication-provider ref="ldapAuthProvider"  / -->#<security:authentication-provider ref="ldapAuthProvider"/>#g' zafira-ws/WEB-INF/spring-security.xml
+  cd zafira-ws && zip -r ../zafira-ws.war * && cd ..
+fi
 
 # Place WAR to webapps
 cp zafira-ws.war $CATALINA_HOME/webapps/zafira-ws.war
@@ -23,5 +32,12 @@ echo zafira.admin.password=$ZAFIRA_PASS >> $CATALINA_HOME/conf/catalina.properti
 echo zafira.db.jdbc.url=$ZAFIRA_JDBC_URL >> $CATALINA_HOME/conf/catalina.properties
 echo zafira.db.jdbc.user=$ZAFIRA_JDBC_USER >> $CATALINA_HOME/conf/catalina.properties
 echo zafira.db.jdbc.password=$ZAFIRA_JDBC_PASS >> $CATALINA_HOME/conf/catalina.properties
+echo zafira.ldap.protocol=$ZAFIRA_LDAP_PROTOCOL >> $CATALINA_HOME/conf/catalina.properties
+echo zafira.ldap.server=$ZAFIRA_LDAP_SERVER >> $CATALINA_HOME/conf/catalina.properties
+echo zafira.ldap.port=$ZAFIRA_LDAP_PORT >> $CATALINA_HOME/conf/catalina.properties
+echo zafira.ldap.dn=$ZAFIRA_LDAP_DN >> $CATALINA_HOME/conf/catalina.properties
+echo zafira.ldap.search_filter=$ZAFIRA_LDAP_SEARCH_FILTER >> $CATALINA_HOME/conf/catalina.properties
+echo zafira.ldap.manager.user=$ZAFIRA_LDAP_USER >> $CATALINA_HOME/conf/catalina.properties
+echo zafira.ldap.manager.password=$ZAFIRA_LDAP_PASSWORD >> $CATALINA_HOME/conf/catalina.properties
 
 catalina.sh run
