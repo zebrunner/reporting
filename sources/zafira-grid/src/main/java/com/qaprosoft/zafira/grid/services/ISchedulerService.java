51238e3c5dfd633b55eb6c1addbbf53dd36b6130
akhursevich <hursrevich@gmail.com>
2017-12-05 15:39:21 +0300

    - dependency optimization

--- sources/zafira-grid/src/main/java/com/qaprosoft/zafira/grid/services/ISchedulerService.java
+++
@@ -1,8 +0,0 @@
-package com.qaprosoft.zafira.grid.services;
-
-public interface ISchedulerService
-{
-	 public void executeGridHealthCheckTask();
-	 
-	 public void executeUsbDeviceHealthCheck();
-}
