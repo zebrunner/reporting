b03da64ff7ca7a10a9810f824fd35415d28cfd06
Aliaksei Khursevich <hursrevich@gmail.com>
2017-12-28 11:29:41 +0300

    - integration tests initial commit

---
+++ sources/zafira-tests/src/main/java/com/qaprosoft/zafira/tests/gui/AbstractPage.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright 2013-2018 QaProSoft (http://www.qaprosoft.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package com.qaprosoft.zafira.tests.gui;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import com.qaprosoft.zafira.tests.util.Config;
+
+public abstract class AbstractPage
+{
+	protected String url;
+	protected WebDriver driver;
+
+	public AbstractPage(WebDriver driver, String path)
+	{
+		this.driver = driver;
+		this.url = Config.get("base_url") + path;
+		PageFactory.initElements(driver, this);
+	}
+	
+	public void open()
+	{
+		driver.get(url);
+	}
+	
+	public boolean isOpened()
+	{
+		return driver.getCurrentUrl().contains(url);
+	}
+}
