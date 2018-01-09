054982794fa6716e5b242a4fa6b2ed570547d70b
brutskov <brutskov@lendingclub.com>
2018-01-09 21:18:32 +0300

    add isElementPresent and waitForPageLoading methods

--- sources/zafira-tests/src/main/java/com/qaprosoft/zafira/tests/gui/LoginPage.java
+++ sources/zafira-tests/src/main/java/com/qaprosoft/zafira/tests/gui/LoginPage.java
@@ -22,7 +22,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class LoginPage extends AbstractPage
+public class LoginPage extends BasePage
 {
 	@FindBy(name="username")
 	private WebElement usernameTextField;
