38e2978e6f3a0ed2bbd6fc1db7af0843236c606c
brutskov <brutskov@lendingclub.com>
2018-01-09 17:56:23 +0300

    add test /test run services for testing

---
+++ sources/zafira-tests/src/main/java/com/qaprosoft/zafira/tests/models/TestRunViewType.java
@@ -0,0 +1,39 @@
+package com.qaprosoft.zafira.tests.models;
+
+import com.qaprosoft.zafira.models.dto.TestRunType;
+import com.qaprosoft.zafira.models.dto.TestType;
+
+import java.util.List;
+
+public class TestRunViewType
+{
+
+	private TestRunType testRunType;
+	private List<TestType> testTypes;
+
+	public TestRunViewType(TestRunType testRunType, List<TestType> testTypes)
+	{
+		this.testRunType = testRunType;
+		this.testTypes = testTypes;
+	}
+
+	public TestRunType getTestRunType()
+	{
+		return testRunType;
+	}
+
+	public void setTestRunType(TestRunType testRunType)
+	{
+		this.testRunType = testRunType;
+	}
+
+	public List<TestType> getTestTypes()
+	{
+		return testTypes;
+	}
+
+	public void setTestTypes(List<TestType> testTypes)
+	{
+		this.testTypes = testTypes;
+	}
+}
---
+++ sources/zafira-tests/src/main/java/com/qaprosoft/zafira/tests/services/TestRunAPIService.java
@@ -0,0 +1,59 @@
+package com.qaprosoft.zafira.tests.services;
+
+import com.qaprosoft.zafira.models.db.Status;
+import com.qaprosoft.zafira.models.dto.TestRunType;
+import com.qaprosoft.zafira.models.dto.TestType;
+import com.qaprosoft.zafira.tests.models.TestRunViewType;
+import com.qaprosoft.zafira.tests.services.builders.TestRunTypeBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.IntStream;
+
+public class TestRunAPIService extends AbstractAPIService
+{
+
+	private TestAPIService testAPIService = new TestAPIService();
+
+	public TestRunViewType createTestRun(TestRunTypeBuilder testRunTypeBuilder, Supplier<List<TestType>> testTypeSupplier)
+	{
+		List<TestType> testTypes = testTypeSupplier.get();
+		return new TestRunViewType(finishTestRun(testRunTypeBuilder), testTypes);
+	}
+
+	public TestRunViewType createTestRun(Integer passedCount, Integer failedCount, Integer inProgressCount, Integer skippedCount,
+			Integer abortedCount)
+	{
+		TestRunTypeBuilder testRunTypeBuilder = new TestRunTypeBuilder();
+		List<TestType> testTypes = new ArrayList<>();
+		return createTestRun(testRunTypeBuilder, () -> {
+			testTypes.addAll(testAPIService.createTests(testRunTypeBuilder, passedCount, Status.PASSED));
+			testTypes.addAll(testAPIService.createTests(testRunTypeBuilder, failedCount, Status.FAILED));
+			testTypes.addAll(testAPIService.createTests(testRunTypeBuilder, skippedCount, Status.SKIPPED));
+			testTypes.addAll(testAPIService.createTests(testRunTypeBuilder, abortedCount, Status.ABORTED));
+			testTypes.addAll(testAPIService.createTests(testRunTypeBuilder, inProgressCount, Status.IN_PROGRESS));
+			return testTypes;
+		});
+	}
+
+	public List<TestRunViewType> createTestRuns(Integer count, Integer boundPassedCount, Integer boundFailedCount, Integer boundInProgressCount,
+			Integer boundSkippedCount, Integer boundAbortedCount)
+	{
+		List<TestRunViewType> result = new ArrayList<>();
+		IntStream.iterate(0, i -> i++).limit(count).parallel().forEach(index -> {
+			result.add(createTestRun(random.nextInt(boundPassedCount), random.nextInt(boundFailedCount),
+					random.nextInt(boundInProgressCount), random.nextInt(boundSkippedCount), random.nextInt(boundAbortedCount)));
+		});
+		return result;
+	}
+
+	private TestRunType finishTestRun(TestRunTypeBuilder testRunTypeBuilder)
+	{
+		TestRunType testRunType = testRunTypeBuilder.getTestRunType();
+		testRunType = ZAFIRA_CLIENT.updateTestRun(testRunType).getObject();
+		testRunType = ZAFIRA_CLIENT.finishTestRun(testRunType.getId()).getObject();
+		ZAFIRA_CLIENT.sendTestRunReport(testRunType.getId(), USER.getEmail(), false, false);
+		return testRunType;
+	}
+}
