c79f562a07ad7fabd070ab2cb356ffc5b41c170d
brutskov <brutskov@lendingclub.com>
2018-01-09 21:18:32 +0300

    add isElementPresent and waitForPageLoading methods

--- sources/zafira-tests/src/main/java/com/qaprosoft/zafira/tests/gui/AbstractPage.java
+++ sources/zafira-tests/src/main/java/com/qaprosoft/zafira/tests/gui/AbstractPage.java
@@ -15,7 +15,11 @@
  *******************************************************************************/
 package com.qaprosoft.zafira.tests.gui;
 
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -24,6 +28,8 @@ import com.qaprosoft.zafira.tests.util.Config;
 
 public abstract class AbstractPage
 {
+	protected static final Logger LOGGER = Logger.getLogger(AbstractPage.class);
+
 	protected String url;
 	
 	protected WebDriver driver;
@@ -38,6 +44,7 @@ public abstract class AbstractPage
 	public void open()
 	{
 		driver.get(url);
+		driver.manage().window().maximize();
 	}
 	
 	public boolean isOpened()
