d4dba934d1d35fe31ffa651b8a7ee6645d399ca0
brutskov <brutskov@lendingclub.com>
2018-01-11 13:08:03 +0300

    add default find methods

--- sources/zafira-tests/src/main/java/com/qaprosoft/zafira/tests/gui/components/IElement.java
+++ sources/zafira-tests/src/main/java/com/qaprosoft/zafira/tests/gui/components/IElement.java
@@ -3,10 +3,22 @@ package com.qaprosoft.zafira.tests.gui.components;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
+
 public interface IElement
 {
 
 	By getLocator();
 
 	WebElement getElement();
+
+	default WebElement findElement(By by)
+	{
+		return getElement().findElement(by);
+	}
+
+	default List<WebElement> findElements(By by)
+	{
+		return getElement().findElements(by);
+	}
 }
