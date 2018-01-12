d0eea0af2a4acd54bf1ab4c755688dcf856bd1b0
tsvrko <UCs7JCmP>
2018-01-12 15:22:32 +0300

    zafira color schema check test

--- sources/zafira-tests/src/test/java/com/qaprosoft/zafira/tests/UserProfileTest.java
+++ sources/zafira-tests/src/test/java/com/qaprosoft/zafira/tests/UserProfileTest.java
@@ -3,14 +3,12 @@ package com.qaprosoft.zafira.tests;
 import com.qaprosoft.zafira.tests.gui.pages.DashboardPage;
 import com.qaprosoft.zafira.tests.gui.pages.LoginPage;
 import com.qaprosoft.zafira.tests.gui.pages.UserProfilePage;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import static com.qaprosoft.zafira.tests.gui.pages.UserProfilePage.ColorSchema;
 
 public class UserProfileTest extends AbstractTest {
 
@@ -38,4 +36,24 @@ public class UserProfileTest extends AbstractTest {
     public void testCopyToken() {
         Assert.assertTrue(userProfilePage.copyToken());
     }
+
+    @Test
+    public void testColorSchemaChange() {
+        ColorSchema colorSchema = userProfilePage.checkCurrentColorSchemeByRadioButton();
+        if (colorSchema == ColorSchema.LIGHT){
+            Assert.assertTrue(userProfilePage.lightSchemaStyleIsDisplayed());
+            Assert.assertTrue(userProfilePage.lightSchemaStyleIsDisplayed());
+            userProfilePage.pickDarkSchemaRadioButton();
+            Assert.assertTrue(userProfilePage.darkSchemaStyleIsDisplayed());
+            userProfilePage.reload();
+            Assert.assertTrue(userProfilePage.lightSchemaStyleIsDisplayed());
+        } else if (colorSchema == ColorSchema.DARK){
+            Assert.assertTrue(userProfilePage.darkSchemaStyleIsDisplayed());
+            userProfilePage.pickLightSchemaRadioButton();
+            Assert.assertTrue(userProfilePage.lightSchemaStyleIsDisplayed());
+            userProfilePage.reload();
+            Assert.assertTrue(userProfilePage.darkSchemaStyleIsDisplayed());
+        }
+    }
+
 }
