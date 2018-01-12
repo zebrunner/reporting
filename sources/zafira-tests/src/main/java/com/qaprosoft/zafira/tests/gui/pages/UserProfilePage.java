9d98d14bf651edab686893c7dbab77d87b39577d
tsvrko <UCs7JCmP>
2018-01-12 15:22:32 +0300

    zafira color schema check test

--- sources/zafira-tests/src/main/java/com/qaprosoft/zafira/tests/gui/pages/UserProfilePage.java
+++ sources/zafira-tests/src/main/java/com/qaprosoft/zafira/tests/gui/pages/UserProfilePage.java
@@ -1,5 +1,6 @@
 package com.qaprosoft.zafira.tests.gui.pages;
 
+import com.qaprosoft.zafira.tests.exceptions.NoColorSchemaIsChosenException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -22,9 +23,59 @@ public class UserProfilePage extends BasePage
 	@FindBy(xpath = "//div[contains(@class, 'ajs-success')]")
 	private WebElement copyActionAlert;
 
+	@FindBy(xpath="//body[contains(@class, 'zaf-light')]")
+	private WebElement lightZafiraSchemaStyle;
+
+	@FindBy(xpath="//body[contains(@class, 'zaf-dark')]")
+	private WebElement darkZafiraSchemaStyle;
+
+	@FindBy(xpath="//md-radio-button[@value=32]")
+	private WebElement lightZafiraSchemaRadioButton;
+
+	@FindBy(xpath="//md-radio-button[@value=22]")
+	private WebElement darkZafiraSchemaRadioButton;
+
+	@FindBy(xpath="//md-radio-button[@value=32 and contains(@class, 'md-checked')]")
+	private WebElement lightZafiraSchemaRadioButtonChecked;
+
+	@FindBy(xpath="//md-radio-button[@value=22 and contains(@class, 'md-checked')]")
+	private WebElement darkZafiraSchemaRadioButtonChecked;
+
+	public enum ColorSchema {LIGHT, DARK};
+
 	public UserProfilePage(WebDriver driver)
 	{
 		super(driver, "/users/profile");
+	}
+
+
+	public ColorSchema checkCurrentColorSchemeByRadioButton(){
+		this.waitUntilPageIsLoaded(4);
+		if(lightZafiraSchemaRadioButtonChecked != null){
+			return ColorSchema.LIGHT;
+		}
+		else if (darkZafiraSchemaRadioButtonChecked != null){
+			return ColorSchema.DARK;
+		}
+		else {
+			throw new NoColorSchemaIsChosenException("No radioButton corresponding to the schema choice was found");
+		}
+	}
+
+	public void pickDarkSchemaRadioButton(){
+		darkZafiraSchemaRadioButton.click();
+	}
+
+	public void pickLightSchemaRadioButton(){
+		lightZafiraSchemaRadioButton.click();
+	}
+
+	public boolean darkSchemaStyleIsDisplayed(){
+		return darkZafiraSchemaStyle != null;
+	}
+
+	public boolean lightSchemaStyleIsDisplayed(){
+		return lightZafiraSchemaStyle != null;
 	}
 
 	public void generateToken()
@@ -48,5 +99,41 @@ public class UserProfilePage extends BasePage
 	public WebElement getTokenInput()
 	{
 		return tokenInput;
+	}
+
+	public WebElement getGenerateTokenButton() {
+		return generateTokenButton;
+	}
+
+	public WebElement getCopyTokenButton() {
+		return copyTokenButton;
+	}
+
+	public WebElement getCopyActionAlert() {
+		return copyActionAlert;
+	}
+
+	public WebElement getLightZafiraSchemaStyle() {
+		return lightZafiraSchemaStyle;
+	}
+
+	public WebElement getDarkZafiraSchemaStyle() {
+		return darkZafiraSchemaStyle;
+	}
+
+	public WebElement getLightZafiraSchemaRadioButton() {
+		return lightZafiraSchemaRadioButton;
+	}
+
+	public WebElement getDarkZafiraSchemaRadioButton() {
+		return darkZafiraSchemaRadioButton;
+	}
+
+	public WebElement getLightZafiraSchemaRadioButtonChecked() {
+		return lightZafiraSchemaRadioButtonChecked;
+	}
+
+	public WebElement getDarkZafiraSchemaRadioButtonChecked() {
+		return darkZafiraSchemaRadioButtonChecked;
 	}
 }
