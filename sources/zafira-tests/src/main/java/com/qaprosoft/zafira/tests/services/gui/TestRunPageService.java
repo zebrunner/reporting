930e5c91df0f9890cb0d87024ea21e32acfca801
brutskov <brutskov@lendingclub.com>
2018-01-31 17:21:40 +0300

    delete unused libraries imports

--- sources/zafira-tests/src/main/java/com/qaprosoft/zafira/tests/services/gui/TestRunPageService.java
+++ sources/zafira-tests/src/main/java/com/qaprosoft/zafira/tests/services/gui/TestRunPageService.java
@@ -1,11 +1,7 @@
 package com.qaprosoft.zafira.tests.services.gui;
 
 import com.qaprosoft.zafira.tests.gui.pages.TestRunPage;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-
-import java.util.Arrays;
 
 public class TestRunPageService extends AbstractPageService
 {
