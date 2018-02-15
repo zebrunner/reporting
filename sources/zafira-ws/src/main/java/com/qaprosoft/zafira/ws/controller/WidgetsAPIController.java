9fd40bba11b3dbe83019f86e5ceff536ea1e68a6
tsvrko <UCs7JCmP>
2018-02-15 13:16:08 +0300

    #{hashcode} placeholder and dynamic Failures Analysis board id replacement

--- sources/zafira-ws/src/main/java/com/qaprosoft/zafira/ws/controller/WidgetsAPIController.java
+++ sources/zafira-ws/src/main/java/com/qaprosoft/zafira/ws/controller/WidgetsAPIController.java
@@ -145,7 +145,8 @@ public class WidgetsAPIController extends AbstractController
 				.replaceAll("#\\{dashboardName\\}", !StringUtils.isEmpty(dashboardName) ? dashboardName : "")
 				.replaceAll("#\\{currentUserId\\}", !StringUtils.isEmpty(currentUserId) ? currentUserId : String.valueOf(getPrincipalId()))
 				.replaceAll("#\\{currentUserName\\}", String.valueOf(getPrincipalName()))
-				.replaceAll("#\\{zafiraURL\\}", zafiraURL);
+				.replaceAll("#\\{zafiraURL\\}", zafiraURL)
+					.replaceAll("#\\{hashcode\\}", "0");
 
 			String param = StringUtils.substringBetween(query,"#{","}%" );
 			if(param != null && !param.equals("project") && !param.equals("dashboardName") && !param.equals("currentUserId") && !param.equals("currentUserName"))
