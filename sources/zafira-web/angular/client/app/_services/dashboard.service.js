ef9010aecd1967c31089c950da668b82412c6ccb
boniara <rutkovba@gmail.com>
2018-03-23 23:26:03 +0300

    fix path encoding bug

--- sources/zafira-web/angular/client/app/_services/dashboard.service.js
+++ sources/zafira-web/angular/client/app/_services/dashboard.service.js
@@ -96,7 +96,7 @@
 
         function SendDashboardByEmail(email, projects) {
         	var config = {'headers' : { 'Access-Token' : $rootScope.globals.auth.accessToken } };
-            return $http.post(API_URL + '/api/dashboards/email?projects=' + JSON.stringify(projects), email, config).then(UtilService.handleSuccess, UtilService.handleError('Unable to send dashboard by email'));
+            return $http.post(API_URL + '/api/dashboards/email?projects=' + encodeURIComponent(JSON.stringify(projects)), email, config).then(UtilService.handleSuccess, UtilService.handleError('Unable to send dashboard by email'));
         }
 
         function GetWidgets() {
