6ed5422fd19668055199e2c700d9d4807b78261e
boniara <rutkovba@gmail.com>
2018-03-23 11:36:05 +0300

    add company logo url cookie setter add company logo rotation image instead of basic animation add company logo to login page delete company logo loading with extended profile

--- sources/zafira-ws/src/main/java/com/qaprosoft/zafira/ws/controller/UsersAPIController.java
+++ sources/zafira-ws/src/main/java/com/qaprosoft/zafira/ws/controller/UsersAPIController.java
@@ -22,9 +22,7 @@ import java.util.Map;
 import javax.validation.Valid;
 
 import com.qaprosoft.zafira.services.services.DashboardService;
-import com.qaprosoft.zafira.services.services.SettingsService;
 import com.qaprosoft.zafira.services.services.jmx.AmazonService;
-import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -65,9 +63,6 @@ public class UsersAPIController extends AbstractController
 {
 	@Autowired
 	private UserService userService;
-
-	@Autowired
-	private SettingsService settingsService;
 
 	@Autowired
 	DashboardService dashboardService;
@@ -112,7 +107,6 @@ public class UsersAPIController extends AbstractController
 		userType.setPreferences(user.getPreferences());
 		userType.setPermissions(user.getPermissions());
 		extendedUserProfile.put("user", userType);
-		extendedUserProfile.put("companyLogo", settingsService.getSettingByName("COMPANY_LOGO_URL"));
 		dashboardService.setDefaultDashboard(extendedUserProfile, "", "defaultDashboardId");
 		dashboardService.setDefaultDashboard(extendedUserProfile, "User Performance", "performanceDashboardId");
 		dashboardService.setDefaultDashboard(extendedUserProfile, "Personal", "personalDashboardId");
