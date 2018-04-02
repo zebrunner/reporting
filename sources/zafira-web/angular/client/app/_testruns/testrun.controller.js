78c2a4cc488a36f3191c543e9dc8acd3e17464c0
tsvrko <UCs7JCmP>
2018-04-02 20:34:01 +0300

    message text refactoring

--- sources/zafira-web/angular/client/app/_testruns/testrun.controller.js
+++ sources/zafira-web/angular/client/app/_testruns/testrun.controller.js
@@ -1684,7 +1684,7 @@
             issueJiraIdInputIsChanged = true;
             $scope.newIssue.description = '';
             $scope.newIssue.id = null;
-            $scope.newIssue.status = 'N/A';
+            $scope.newIssue.status = null;
             $scope.newIssue.assignee = null;
             $scope.newIssue.reporter = null;
             $scope.issueJiraIdExists = true;
@@ -1705,7 +1705,7 @@
             taskJiraIdInputIsChanged = true;
             $scope.newTask.description = '';
             $scope.newTask.id = null;
-            $scope.newTask.status = 'N/A';
+            $scope.newTask.status = null;
             $scope.newTask.assignee = null;
             $scope.newTask.reporter = null;
             $scope.taskJiraIdExists = true;
