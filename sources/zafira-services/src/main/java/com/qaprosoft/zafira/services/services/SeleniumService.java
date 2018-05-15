b7f9117534c1e0020671aa71daf1e220904a4b63
Aliksei Khursevich <hursevich@gmail.com>
2018-05-15 12:40:22 +0300

    - chrome driver init updated

--- sources/zafira-services/src/main/java/com/qaprosoft/zafira/services/services/SeleniumService.java
+++ sources/zafira-services/src/main/java/com/qaprosoft/zafira/services/services/SeleniumService.java
@@ -21,7 +21,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.attribute.PosixFilePermission;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
 
@@ -33,10 +38,15 @@ import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.SystemUtils;
 import org.apache.log4j.Logger;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
@@ -46,90 +56,86 @@ import com.qaprosoft.zafira.services.exceptions.ServiceException;
 import com.qaprosoft.zafira.services.util.WebDriverUtil;
 
 @Service
-public class SeleniumService 
+public class SeleniumService
 {
 	private Logger LOGGER = Logger.getLogger(EmailService.class);
-	
+
 	private static final Dimension DEFAULT_SCREEN_DIMENSION = new Dimension(1920, 1080);
-	
+
 	private static final String MAC_CHROMEDRIVER = "classpath:chromedriver/chromedriver-macos";
-	
+
 	private static final String WINDOWS_CHROMEDRIVER = "classpath:chromedriver/chromedriver-windows.exe";
-	
+
 	private static final String LINUX_CHROMEDRIVER = "classpath:chromedriver/chromedriver-linux";
 
 	private File binary;
-	
+
 	@Autowired
 	private ResourceLoader resourceLoader;
 
-     /**
+	/**
 	 * Initializes PhantomJS binary according to OS.
 	 */
 	@PostConstruct
-	public void init() 
+	public void init()
 	{
 		InputStream is = null;
 		try
 		{
-			if (SystemUtils.IS_OS_MAC) 
+			if (SystemUtils.IS_OS_MAC)
 			{
 				is = resourceLoader.getResource(MAC_CHROMEDRIVER).getInputStream();
-			} 
-			else if (SystemUtils.IS_OS_WINDOWS) 
+			} else if (SystemUtils.IS_OS_WINDOWS)
 			{
 				is = resourceLoader.getResource(WINDOWS_CHROMEDRIVER).getInputStream();
-			} 
-			else 
+			} else
 			{
 				is = resourceLoader.getResource(LINUX_CHROMEDRIVER).getInputStream();
 			}
-			
+
 			binary = new File("./chromedriver" + (SystemUtils.IS_OS_WINDOWS ? ".exe" : ""));
 
-			//Files.deleteIfExists(binary.toPath());
-
-			//System.setProperty("webdriver.chrome.driver", bin.getAbsolutePath());
+			Files.deleteIfExists(binary.toPath());
 
 			FileUtils.copyInputStreamToFile(is, binary);
 			IOUtils.closeQuietly(is);
-			
+
 			Set<PosixFilePermission> perms = new HashSet<>();
 			perms.add(PosixFilePermission.OWNER_EXECUTE);
 			perms.add(PosixFilePermission.OWNER_READ);
 			Files.setPosixFilePermissions(binary.toPath(), perms);
 
-		}
-		catch(Exception e)
+		} catch (Exception e)
 		{
 			LOGGER.error("Failed to initialize PhantomJS: " + e.getMessage());
-		}
-		finally
+		} finally
 		{
 			IOUtils.closeQuietly(is);
 		}
 	}
 
-	public List<Attachment> captureScreenshoots(List<String> urls, String domain, String auth, String projects, By areaLocator, By titleLocator, Dimension dimension) throws ServiceException
+	public List<Attachment> captureScreenshoots(List<String> urls, String domain, String auth, String projects,
+			By areaLocator, By titleLocator, Dimension dimension) throws ServiceException
 	{
 		List<Attachment> attachments = new ArrayList<>();
-		
-		if(urls == null || StringUtils.isEmpty(auth))
+
+		if (urls == null || StringUtils.isEmpty(auth))
 		{
 			throw new ServiceException("To capture screenshot specify: urls, auth");
 		}
-		
+
 		WebDriver wd = null;
 		try
 		{
-			DesiredCapabilities caps = new DesiredCapabilities();
-			caps.setJavascriptEnabled(true);
+			final ChromeDriverService service = new ChromeDriverService.Builder()
+					.usingDriverExecutable(binary)
+					.build();
+
 			final ChromeOptions chromeOptions = new ChromeOptions();
 			chromeOptions.addArguments("--headless");
-			chromeOptions.setBinary(binary);
-			caps.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
-	 		wd = new ChromeDriver(caps);
-			
+
+			wd = new ChromeDriver(service, chromeOptions);
+
 			wd.manage().window().setSize(dimension != null ? dimension : DEFAULT_SCREEN_DIMENSION);
 
 			// Get first url to have a domain needed
@@ -140,11 +146,11 @@ public class SeleniumService
 			wd.get(urls.get(0));
 			// Refresh page to enable cookies
 			wd.navigate().refresh();
-			
-			for(String url : urls)
+
+			for (String url : urls)
 			{
 				wd.get(url);
-				if(WebDriverUtil.isPageLoadingWithAnimation(wd) && WebDriverUtil.isPageLoading(wd))
+				if (WebDriverUtil.isPageLoadingWithAnimation(wd) && WebDriverUtil.isPageLoading(wd))
 				{
 					WebDriverUtil.waitUntilPageIsLoaded(wd);
 				} else
@@ -160,21 +166,20 @@ public class SeleniumService
 
 				String name = screenshotDocument.getName();
 
-                if(titleLocator != null)
-                {
-                    name = wd.findElement(titleLocator).getAttribute("value");
-                }
+				if (titleLocator != null)
+				{
+					name = wd.findElement(titleLocator).getAttribute("value");
+				}
 
 				attachments.add(new Attachment(name, screenshotDocument));
 			}
-		}
-		catch(Exception e)
+		} catch (Exception e)
 		{
 			LOGGER.error(e.getMessage());
-		}
-		finally
+		} finally
 		{
-			if(wd != null) wd.quit();
+			if (wd != null)
+				wd.quit();
 		}
 		return attachments;
 	}
@@ -182,7 +187,8 @@ public class SeleniumService
 	public void hideElements(WebDriver wd, WebElement... webElements)
 	{
 		JavascriptExecutor js = (JavascriptExecutor) wd;
-		Arrays.asList(webElements).forEach(we -> js.executeScript("arguments[0].setAttribute('style', 'opacity:0')", we));
+		Arrays.asList(webElements)
+				.forEach(we -> js.executeScript("arguments[0].setAttribute('style', 'opacity:0')", we));
 	}
 
 	private File saveImage(final BufferedImage screenshot) throws IOException
@@ -192,18 +198,19 @@ public class SeleniumService
 		ImageIO.write(screenshot, "png", screenshotDocument);
 		return screenshotDocument;
 	}
-	
+
 	private String normalizeDomain(String domain)
 	{
-		if(!Pattern.matches("\\d+.\\d+.\\d+.\\d+", domain) && Pattern.matches("([A-z0-9-]+\\.)+[A-z0-9]+", domain))
+		if (!Pattern.matches("\\d+.\\d+.\\d+.\\d+", domain) && Pattern.matches("([A-z0-9-]+\\.)+[A-z0-9]+", domain))
 		{
 			String[] sd = domain.split("\\.");
 			domain = "." + sd[sd.length - 2] + "." + sd[sd.length - 1];
 		}
 		return domain;
 	}
-	
-	private void authorize(WebDriver wd, String auth, String projects, String domain, String url) throws InterruptedException
+
+	private void authorize(WebDriver wd, String auth, String projects, String domain, String url)
+			throws InterruptedException
 	{
 		wd.manage().addCookie(new Cookie.Builder("Access-Token", auth).domain(normalizeDomain(domain)).build());
 		wd.manage().addCookie(new Cookie.Builder("projects", projects).domain(normalizeDomain(domain)).build());
