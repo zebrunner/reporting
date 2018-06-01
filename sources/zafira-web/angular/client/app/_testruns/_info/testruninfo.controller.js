1c1312ae548cb9e79dc3bbe797098d9de20ddd3d
boniara <rutkovba@gmail.com>
2018-06-01 15:40:48 +0300

    add vnc

--- sources/zafira-web/angular/client/app/_testruns/_info/testruninfo.controller.js
+++ sources/zafira-web/angular/client/app/_testruns/_info/testruninfo.controller.js
@@ -3,10 +3,10 @@
 
     angular
         .module('app.testruninfo')
-        .controller('TestRunInfoController', ['$scope', '$log', '$q', 'TestService', 'TestRunService', '$stateParams', TestRunInfoController])
+        .controller('TestRunInfoController', ['$scope', '$log', '$timeout', '$window', '$q', 'TestService', 'TestRunService', '$stateParams', TestRunInfoController])
 
     // **************************************************************************
-    function TestRunInfoController($scope, $log, $q, TestService, TestRunService, $stateParams) {
+    function TestRunInfoController($scope, $log, $timeout, $window, $q, TestService, TestRunService, $stateParams) {
 
         $scope.testRun = {};
         $scope.test = {};
@@ -41,6 +41,64 @@
             });
         };
 
+        var rfb;
+        var display;
+        var ratio;
+
+        $scope.loading = true;
+
+        var container  = angular.element('video')[0];
+        var containerHeightProperty = 'offsetHeight';
+        var containerWidthProperty = 'offsetWidth';
+
+        $scope.initVNCWebsocket = function(wsURL) {
+            rfb = new RFB(angular.element('#vnc')[0], wsURL, { shared: true, credentials: { password: 'selenoid' } });
+            //rfb._viewOnly = true;
+            rfb.addEventListener("connect",  connected);
+            rfb.addEventListener("disconnect",  disconnected);
+            rfb.scaleViewport = true;
+            rfb.resizeSession = true;
+            display = rfb._display;
+            display._scale = 1;
+            angular.element($window).bind('resize', function(){
+                autoscale(display, ratio, container);
+            });
+        };
+
+        function connected(e) {
+            $scope.loading = false;
+            var canvas = document.getElementsByTagName("canvas")[0];
+            ratio = canvas.width / canvas.height;
+            autoscale(display, ratio, container);
+
+        };
+
+        function disconnected(e) {
+            $scope.hide();
+        };
+
+        function autoscale(display, ratio, window) {
+            var size = calculateSize(window, ratio);
+            display.autoscale(size.width, size.height, false);
+        };
+
+        function calculateSize(window, ratio) {
+            var width = window[containerWidthProperty];
+            var height = ratio > 1 ?  width / ratio : width * ratio;
+            if(height > window[containerHeightProperty])
+            {
+                height = window[containerHeightProperty] - 100;
+                width = ratio < 1 ? height / ratio : height * ratio;
+            }
+            return {height: height, width: width};
+        };
+
+        $scope.$on('$destroy', function () {
+            if(rfb && rfb._connected) {
+                rfb.disconnect();
+            }
+        });
+
         (function init() {
             getTestRun($stateParams.id).then(function (rs) {
                 getTest(rs.id).then(function (testsRs) {
@@ -48,6 +106,11 @@
                     $scope.test = testsRs.filter(function (t) {
                         return t.id === parseInt($stateParams.testId);
                     })[0];
+                    var videoArtifacts = $scope.test.artifacts ? $scope.test.artifacts.filter(function (artifact) {
+                        return artifact.name.toLowerCase().includes('live demo')
+                    }) : [];
+                    $scope.wsURL = videoArtifacts && videoArtifacts.length ? videoArtifacts[0].link : undefined;
+                    $scope.initVNCWebsocket($scope.wsURL);
                     $scope.testRun.tests = testsRs;
                 });
             });
