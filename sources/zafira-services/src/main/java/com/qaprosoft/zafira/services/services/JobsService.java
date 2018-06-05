2e6fb3bb5f4a0a264cc358642b4fc797ba7a27d1
tsvrko <UCs7JCmP>
2018-06-05 13:24:36 +0300

    parsing job from url with the last char '/' fixed

--- sources/zafira-services/src/main/java/com/qaprosoft/zafira/services/services/JobsService.java
+++ sources/zafira-services/src/main/java/com/qaprosoft/zafira/services/services/JobsService.java
@@ -48,6 +48,9 @@ public class JobsService
 	@Transactional(rollbackFor = Exception.class)
 	public Job createOrUpdateJobByURL(String jobUrl, User user) throws ServiceException
 	{
+		if(jobUrl.charAt(jobUrl.length() - 1) == '/'){
+            jobUrl = StringUtils.chop(jobUrl);
+		}
 		String jobName = StringUtils.substringAfterLast(jobUrl, "/");
 		String jenkinsHost = StringUtils.EMPTY;
 		if(jobUrl.contains("/view/"))
