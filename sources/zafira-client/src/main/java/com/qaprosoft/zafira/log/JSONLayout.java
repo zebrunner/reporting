de10169c1c8e6c424e091bcb3dc74c0205a2981a
boniara <rutkovba@gmail.com>
2018-08-01 14:54:20 +0300

    added possibility to recognize base64 images in logs and transfer to rabbitmq in appropriate structure

--- sources/zafira-client/src/main/java/com/qaprosoft/zafira/log/JSONLayout.java
+++ sources/zafira-client/src/main/java/com/qaprosoft/zafira/log/JSONLayout.java
@@ -101,10 +101,11 @@ public class JSONLayout extends Layout
 	 */
 	protected void writeBasic(JSONObject json, LoggingEvent event) throws JSONException
 	{
+		boolean isImageLog = ZafiraLogAppender.isImageLog(event);
 		json.put("threadName", event.getThreadName());
 		json.put("level", event.getLevel().toString());
 		json.put("timestamp", System.currentTimeMillis());
-		json.put("message", event.getMessage());
+		json.put(isImageLog ? "blob" : "message", isImageLog ? ZafiraLogAppender.getBase64String(event) : event.getMessage());
 		json.put("logger", event.getLoggerName());
 	}
 
