cc6e4f42e528d7c0f9fb1ccf9c814d4641b8f8b0
Aliksei Khursevich <hursevich@gmail.com>
2018-08-08 18:32:03 +0300

    - dockerfile hotfix

---
+++ tools/scripts/docker.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+mvn -f sources/pom.xml clean:clean install -Dwtpversion=2.0 -DskipTests -P zafira
+mvn -f sources/zafira-web/pom.xml war:war -P zafira
+docker build -t qaprosoft/zafira:rabbitmq .
+docker-compose up
