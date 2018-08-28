e419b3f615f016220bc49d1c7f8a32a8d59ee9b6
boniara <rutkovba@gmail.com>
2018-08-28 17:49:34 +0300

    fix company logo editting bug

--- sources/zafira-web/angular/client/app/core/app.controller.js
+++ sources/zafira-web/angular/client/app/core/app.controller.js
@@ -229,7 +229,8 @@
                     if(rs.success)
                     {
                         if(! $rootScope.companyLogo.value || $rootScope.companyLogo.value != rs.data) {
-                            $rootScope.companyLogo.value = rs.data;
+                            $rootScope.companyLogo.value = rs.data.value;
+                            $rootScope.companyLogo.id = rs.data.id;
                             SettingProvider.setCompanyLogoURL($rootScope.companyLogo.value);
                         }
                     }
