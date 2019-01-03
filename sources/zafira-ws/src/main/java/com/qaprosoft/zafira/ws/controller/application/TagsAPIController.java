99c236605586e67744ddccadf39e4d90e474676a
tsvrko <tsvrko@gmail.com>
2019-01-03 19:07:17 +0300

    new testRailRunName

--- sources/zafira-ws/src/main/java/com/qaprosoft/zafira/ws/controller/application/TagsAPIController.java
+++ sources/zafira-ws/src/main/java/com/qaprosoft/zafira/ws/controller/application/TagsAPIController.java
@@ -81,6 +81,9 @@ public class TagsAPIController extends AbstractController
             integration.setTestRunName(testRun.getName(configMap));
             switch (integrationTag){
                 case TESTRAIL_TESTCASE_UUID:
+                    String testRailRunName = "%s. %s: %s";
+                    String.format(testRailRunName, testRun.getPlatform(), testRun.getTestSuite().getFileName(), testRun.getAppVersion());
+                    integration.setTestRunName(testRailRunName);
                     configuration.getArg().forEach(arg -> {
                         if(arg.getKey().contains("testrail_assignee")){
                             integration.getCustomParams().put("assignee", arg.getValue());
