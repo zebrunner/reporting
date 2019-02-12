7586b451c45c93ae0c81ffc4cc9a093aa0725d36
tsvrko <tsvrko@gmail.com>
2019-02-12 17:43:01 +0300

    redundant import is removed

--- sources/zafira-services/src/main/java/com/qaprosoft/zafira/services/services/application/jmx/JenkinsService.java
+++ sources/zafira-services/src/main/java/com/qaprosoft/zafira/services/services/application/jmx/JenkinsService.java
@@ -31,7 +31,6 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.http.client.HttpResponseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
