758c80a2d71237a3eebf401c509f17b8050c32b4
Bogdan Rutskov <rutkovba@gmail.com>
2019-03-06 09:51:36 +0300

    es client using native angularjs was created

--- sources/zafira-web/angular/client/app/_services/elasticsearch.service.js
+++ sources/zafira-web/angular/client/app/_services/elasticsearch.service.js
@@ -3,11 +3,13 @@
 
     angular
         .module('app.services')
-        .factory('ElasticsearchService', ['$q', 'esFactory', '$location', 'SettingsService', '$rootScope', ElasticsearchService])
+        .factory('ElasticsearchService', ['$q', '$http', 'esFactory', '$location', 'SettingsService', '$rootScope', ElasticsearchService])
 
-    function ElasticsearchService($q, esFactory, $location, SettingsService, $rootScope) {
+    function ElasticsearchService($q, $http, esFactory, $location, SettingsService, $rootScope) {
 
         var instance;
+
+        var instanceV2;
 
         var service = {};
 
@@ -152,7 +154,7 @@
                             return element.name.toLowerCase() == 'password';
                         });
                         if (url && url.value) {
-                            resolve(createInstance(url.value, user, password));
+                            resolve(createInstanceV2(url.value, user, password));
                         } else {
                             reject({
                                 errorMessage: 'Cannot initialize elasticsearch url'
@@ -187,6 +189,142 @@
                     }
                 });
             }
+        };
+
+        function createInstanceV2(url, user, password) {
+            if(instanceV2)
+                return instanceV2;
+
+            instanceV2 = {};
+            instanceV2.url = url;
+            instanceV2.user = user;
+            instanceV2.password = password;
+            instanceV2.basic = getAuthorizationValue(user.value, password.value);
+            if(url.includes('@') && ! user.value && ! password.value && ! user.value.length && ! password.value.length) {
+                var protocol_auth_slices = url.split('@')[0].split('://');
+                if(protocol_auth_slices.length === 2 && protocol_auth_slices[1].includes(':')) {
+                    var username_password_slices = protocol_auth_slices[1].split(':');
+                    if(username_password_slices.length === 2) {
+                        instanceV2.basic = getAuthorizationValue(username_password_slices[0], username_password_slices[1]);
+                    }
+                }
+            }
+
+            instanceV2.search = function (params, callback) {
+                apply(buildHttpRequest(url, params.index, 'SEARCH', params.body, user, password), callback);
+            };
+            instanceV2.count = function (params, callback) {
+                apply(buildHttpRequest(url, params.index, 'COUNT', params.body, user, password), callback);
+            };
+            instanceV2.indices = {
+                exists: function (params, callback) {
+                    apply(buildHttpRequest(url, params.index, 'EXISTS', null, user, password), callback);
+                }
+            };
+            return instanceV2;
+        }
+
+        function apply(promise, callback) {
+            promise.then(function (rs) {
+                callback(null, rs.data);
+            }, function (rs) {
+                callback(rs.data, null);
+            });
+        };
+
+        function getAuthorizationValue(username, password) {
+            if(! username || ! password)
+                return;
+            return "Basic " + btoa(username + ":" + password)
+        };
+
+        function getCount(url, params) {
+            return retrieveCount(url, params.index, params.body);
+        };
+
+        function getData(url, params) {
+            return retrieveData(url, params.index, params.body);
+        };
+
+        function getExisting(url, params) {
+            return retrieveExisting(url, params.index);
+        };
+
+        function retrieveCount(url, index, body) {
+            return $http.post(url + '/' + index + '/_count', body).then(UtilService.handleSuccess, UtilService.handleError('Unable to get indices count from elasticsearch'));
+        };
+
+        function retrieveData(url, index, body) {
+            return $http.post(url + '/' + index + '/_search', body).then(UtilService.handleSuccess, UtilService.handleError('Unable to get data from elasticsearch'));
+        };
+
+        function buildHttpRequest(url, index, action, body, username, password) {
+
+            if(! url || ! action)
+                return;
+
+            var postfix = getPostfix(action, index);
+            var request = {
+                url: url + postfix
+            };
+            switch(action) {
+                case 'SEARCH':
+                case 'COUNT':
+                    request.method = 'POST';
+                    break;
+                case 'EXISTS':
+                    request.method = 'HEAD';
+                    break;
+                default:
+                    break;
+            }
+
+            if(! request.method)
+                return;
+
+            if(['POST'].indexOf(request.method) !== -1) {
+                request.data = body;
+            }
+
+            var authorizationValue = instanceV2.basic;
+            if(authorizationValue) {
+                request.headers = {
+                    'Authorization': authorizationValue
+                }
+            }
+
+            return $http(request);
+        };
+
+        function retrieveExisting(url, index) {
+            return $q(function (resolve, reject) {
+                $http.head(url + '/' + index).then(function (rs) {
+                    resolve(true);
+                }, function (rs) {
+                    resolve(false);
+                });
+            });
+        };
+
+        function getPostfix(action, index) {
+            if(! action)
+                return;
+
+            var postfix = '/' + index;
+
+            switch(action) {
+                case 'SEARCH':
+                    postfix = postfix + '/_search';
+                    break;
+                case 'COUNT':
+                    postfix = postfix + '/_count';
+                    break;
+                case 'EXISTS':
+                    break;
+                default:
+                    break;
+            }
+            return postfix;
         };
     }
 })();
