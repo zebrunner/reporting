4411c5821d8c8d30c26b53a1859e3279c9a5605f
tsvrko <tsvrko@gmail.com>
2019-03-26 12:20:07 +0300

    forEach usage is fixed

--- sources/zafira-web/angular/client/app/containers/test-details/test-details.controller.js
+++ sources/zafira-web/angular/client/app/containers/test-details/test-details.controller.js
@@ -213,14 +213,11 @@
         function addTest(test) {
             test.elapsed = test.finishTime ? (test.finishTime - test.startTime) : Number.MAX_VALUE;
             prepareArtifacts(test);
-            test.tags = test.tags.forEach(
-                tag => {
-                    if(tag.name === 'TESTRAIL_TESTCASE_UUID' || tag.name === 'QTEST_TESTCASE_UUID'){
-                        tag.value = tag.value.split('-').pop();
-                    }
+            test.tags = test.tags.forEach(function(tag) {
+                if(tag.name === 'TESTRAIL_TESTCASE_UUID' || tag.name === 'QTEST_TESTCASE_UUID'){
+                    tag.value = tag.value.split('-').pop();
                 }
-            );
-
+            });
             vm.testRun.tests[test.id] = test;
 
             if (vm.testGroupMode === 'PLAIN') {
