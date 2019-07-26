c4d7de6de2175debdc88321306d356e87527986f
tsvrko <tsvrko@gmail.com>
2019-07-26 15:50:05 +0300

    unnecessary changes are reverted

--- start.sh
+++ start.sh
@@ -4,14 +4,14 @@ BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 cd ${BASEDIR}
 
-#num=`sysctl -n vm.max_map_count`
+num=`sysctl -n vm.max_map_count`
 
-#if ((num < 262144));
-#then
-#    echo "Operating system limits on mmap counts is too low, which prevents elasticsearch from starting."
-##    echo "Please, modify the vm.max_map_count setting in /etc/sysctl.conf, set vm.max_map_count=262144"
-#    exit
-#fi
+if ((num < 262144));
+then
+    echo "Operating system limits on mmap counts is too low, which prevents elasticsearch from starting."
+    echo "Please, modify the vm.max_map_count setting in /etc/sysctl.conf, set vm.max_map_count=262144"
+    exit
+fi
 
 if [ ! -d esdata ]; then
   mkdir -p esdata;
