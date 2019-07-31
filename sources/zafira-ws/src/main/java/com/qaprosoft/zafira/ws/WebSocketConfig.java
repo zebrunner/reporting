3f65affcf5303ee8b55a58592a4dde5ee70ca1a9
Bogdan Rutskov <rutkovba@gmail.com>
2019-07-31 18:31:40 +0300

    deprecated web socket spring config implementation was replaced with newly interface

--- sources/zafira-ws/src/main/java/com/qaprosoft/zafira/ws/WebSocketConfig.java
+++ sources/zafira-ws/src/main/java/com/qaprosoft/zafira/ws/WebSocketConfig.java
@@ -18,13 +18,13 @@ package com.qaprosoft.zafira.ws;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     private static final String[] STOMP_ENDPOINTS = new String[]{"/api/websockets"};
     private static final String[] STOMP_BROKER_DESTINATIONS = new String[]{"/topic"};
