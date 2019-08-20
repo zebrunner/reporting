8522face5115a49467e72bd8264183f9b89ac5f5
Nikita Sidorevich <nikitasidorevich@gmail.com>
2019-08-20 17:56:36 +0300

    Unused scripts and configs removed

--- tools/scripts/docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-mvn -f sources/pom.xml clean:clean install -Dwtpversion=2.0 -DskipTests -P zafira
-mvn -f sources/zafira-web/pom.xml war:war -P zafira
-docker build -t qaprosoft/zafira:latest .
-docker-compose up
