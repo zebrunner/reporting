e6407e28d93bb3c820a71c89c0a361f50dfbbbd8
Nikita Sidorevich <nikitasidorevich@gmail.com>
2019-08-27 19:30:31 +0300

    Refactored

--- sources/zafira-services/src/main/java/com/qaprosoft/zafira/services/services/application/integration/tool/context/proxy/AbstractProxy.java
+++ sources/zafira-services/src/main/java/com/qaprosoft/zafira/services/services/application/integration/tool/context/proxy/AbstractProxy.java
@@ -31,7 +31,6 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Parameter;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -42,7 +41,9 @@ public abstract class AbstractProxy {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AbstractProxy.class);
 
-    private static final String ERR_MSG_MANDATORY_INTEGRATION_PARAMS_MISSING = "Integration settings %s are mandatory for integration with name '%s'";
+    private static final String ERR_MSG_MANDATORY_INTEGRATION_PARAMS_MISSING = "Integration settings [%s] are mandatory for integration %s";
+    private static final String ERR_MSG_CANNOT_CREATE_ADAPTER = "Cannot create an instance of %s adapter for integration %s";
+
 
     private static final Map<String, Map<Long, IntegrationAdapter>> TENANT_ADAPTERS = new ConcurrentHashMap<>();
 
@@ -52,11 +53,13 @@ public abstract class AbstractProxy {
     private final Map<String, Class<? extends IntegrationAdapter>> adapterClasses;
     private final Map<String, Object> additionalParameters;
 
-    public AbstractProxy(ApplicationContext applicationContext,
-                         IntegrationGroupService integrationGroupService,
-                         String group,
-                         Map<String, Class<? extends IntegrationAdapter>> adapterClasses,
-                         Map<String, Object> additionalParameters) {
+    public AbstractProxy(
+            ApplicationContext applicationContext,
+            IntegrationGroupService integrationGroupService,
+            String group,
+            Map<String, Class<? extends IntegrationAdapter>> adapterClasses,
+            Map<String, Object> additionalParameters
+    ) {
         this.applicationContext = applicationContext;
         this.integrationGroupService = integrationGroupService;
         this.group = group;
@@ -64,105 +67,115 @@ public abstract class AbstractProxy {
         this.additionalParameters = additionalParameters;
     }
 
-    public AbstractProxy(ApplicationContext applicationContext, IntegrationGroupService integrationGroupService, String group, Map<String, Class<? extends IntegrationAdapter>> adapterClasses) {
+    public AbstractProxy(
+            ApplicationContext applicationContext,
+            IntegrationGroupService integrationGroupService,
+            String group,
+            Map<String, Class<? extends IntegrationAdapter>> adapterClasses
+    ) {
         this(applicationContext, integrationGroupService, group, adapterClasses, null);
     }
 
-    public static synchronized void putAdapter(IntegrationAdapter integrationAdapter) {
-        if (TENANT_ADAPTERS.get(TenancyContext.getTenantName()) == null) {
-            TENANT_ADAPTERS.put(TenancyContext.getTenantName(), new ConcurrentHashMap<>());
+    private static synchronized void putAdapter(IntegrationAdapter integrationAdapter) {
+        String tenant = TenancyContext.getTenantName();
+        if (TENANT_ADAPTERS.get(tenant) == null) {
+            TENANT_ADAPTERS.put(tenant, new ConcurrentHashMap<>());
         }
-        TENANT_ADAPTERS.get(TenancyContext.getTenantName()).put(integrationAdapter.getIntegration().getId(), integrationAdapter);
+
+        Long integrationId = integrationAdapter.getIntegration().getId();
+        TENANT_ADAPTERS.get(tenant).put(integrationId, integrationAdapter);
     }
 
-    public static synchronized Optional<IntegrationAdapter> getAdapter(Long id) {
+    public static synchronized Optional<IntegrationAdapter> getAdapter(Long integrationId) {
         Map<Long, IntegrationAdapter> adapters = TENANT_ADAPTERS.get(TenancyContext.getTenantName());
-        return Optional.ofNullable(adapters.get(id));
+        return Optional.ofNullable(adapters.get(integrationId));
     }
 
     public static synchronized Optional<IntegrationAdapter> getDefaultAdapter(String type) {
         Map<Long, IntegrationAdapter> adapters = TENANT_ADAPTERS.get(TenancyContext.getTenantName());
-        return adapters.values().stream()
-                       .filter(adapter -> adapter.getType().equals(type) && adapter.getIntegration().isDefault())
+        return adapters.values()
+                       .stream()
+                       .filter(adapter -> adapter.getType().equals(type))
+                       .filter(adapter -> adapter.getIntegration().isDefault())
                        .findFirst();
     }
 
     public void init() {
         IntegrationGroup integrationGroup = integrationGroupService.retrieveByName(getGroup());
         List<IntegrationType> integrationTypes = integrationGroup.getIntegrationTypes();
+
         integrationTypes.forEach(integrationType -> {
-            Class<? extends IntegrationAdapter> adapterClass = adapterClasses.get(integrationType.getName());
+
+
             List<Integration> integrations = integrationType.getIntegrations();
 
-            // check if number of enabled integrations is equal to number of valid integrations
-            // if yes - ok, proceed
-            // if no -> log all invalid integrations, fail startup
-
-            /*List<Integration> enabledIntegrations = integrations.stream()
+            // collect all enabled integrations first
+            List<Integration> enabledIntegrations = integrations.stream()
                                                                 .filter(Integration::isEnabled)
                                                                 .collect(Collectors.toList());
 
-            Long numberOfValidIntegrations = enabledIntegrations.stream()
-                                                                .filter(this::isValidIntegration)
-                                                                .count();
+            // make sure all of those have mandatory settings in place
+            long validIntegrationsCount = enabledIntegrations.stream()
+                                                             .filter(this::hasMandatorySettingsSet)
+                                                             .count();
 
-            List<IntegrationAdapter> integrationAdapters = Collections.emptyList();
-            if (enabledIntegrations.size() != numberOfValidIntegrations) {
-                // throw exception, fail startup
+
+            // if not all enabled integrations are properly configured - fail startup, otherwise initialize adapters
+            if (enabledIntegrations.size() != validIntegrationsCount) {
+                //todo add code and message
+                throw new IntegrationException();
             } else {
-                integrationAdapters = enabledIntegrations.stream()
-                                                         .map(integration -> createAdapterInstance(integration, adapterClass))
-                                                         .collect(Collectors.toList());
-
-                integrationAdapters.forEach(AbstractProxy::putAdapter);
-            }*/
-
-
-
-
-            List<IntegrationAdapter> integrationAdapters = integrations.stream()
-                                                                       .filter(Integration::isEnabled)
-                                                                       .filter(this::isValidIntegration)
-                                                                       .map(integration -> createAdapterInstance(integration, adapterClass)).collect(Collectors.toList());
-            integrationAdapters.forEach(AbstractProxy::putAdapter);
+                Class<? extends IntegrationAdapter> adapterClass = adapterClasses.get(integrationType.getName());
+                initializeAdapters(adapterClass, enabledIntegrations);
+            }
         });
     }
 
-    private boolean isValidIntegration(Integration integration) {
-        boolean valid = true;
-        List<IntegrationSetting> integrationSettings = integration.getIntegrationSettings();
-        String mandatoryEmptySettings = integrationSettings.stream()
-                                                           .filter(integrationSetting -> integrationSetting.getIntegrationParam().isMandatory() && integrationSetting.getValue() != null && !integrationSetting.getValue().isBlank())
-                                                           .map(integrationSetting -> integrationSetting.getIntegrationParam().getName())
-                                                           .collect(Collectors.joining(", "));
-
-        if (mandatoryEmptySettings != null && mandatoryEmptySettings.length() != 0) {
-            valid = false;
-            LOGGER.error(String.format(ERR_MSG_MANDATORY_INTEGRATION_PARAMS_MISSING, mandatoryEmptySettings, integration.getName()));
-        }
-
-        return valid;
+    private void initializeAdapters(Class<? extends IntegrationAdapter> adapterClass, List<Integration> enabledIntegrations) {
+        enabledIntegrations.stream()
+                           .map(integration -> createAdapter(integration, adapterClass))
+                           .forEach(AbstractProxy::putAdapter);
     }
 
-    private IntegrationAdapter createAdapterInstance(Integration integration, Class<? extends IntegrationAdapter> adapterClass) {
-        IntegrationAdapter integrationAdapter = null;
+    private boolean hasMandatorySettingsSet(Integration integration) {
+        List<IntegrationSetting> integrationSettings = integration.getIntegrationSettings();
+
+        // go over all mandatory integration settings and check if those have values set
+        String missingSettings = integrationSettings.stream()
+                                                    .filter(integrationSetting -> integrationSetting.getIntegrationParam().isMandatory())
+                                                    .filter(integrationSetting -> integrationSetting.getValue() != null && !integrationSetting.getValue().isBlank())
+                                                    .map(integrationSetting -> integrationSetting.getIntegrationParam().getName())
+                                                    .collect(Collectors.joining(", "));
+
+        if (!missingSettings.isEmpty()) {
+            LOGGER.error(String.format(ERR_MSG_MANDATORY_INTEGRATION_PARAMS_MISSING, missingSettings, integration.getName()));
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private IntegrationAdapter createAdapter(Integration integration, Class<? extends IntegrationAdapter> adapterClass) {
+        IntegrationAdapter adapter = null;
         Constructor constructor = getCorrectConstructor(adapterClass);
         Object[] params = buildParameterObjects(constructor, integration);
         try {
-            integrationAdapter = (IntegrationAdapter) constructor.newInstance(params);
+            adapter = (IntegrationAdapter) constructor.newInstance(params);
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
-            throw new IntegrationException(String.format("Cannot to create %s adapter instance", adapterClass.getSimpleName()));
+            throw new IntegrationException(String.format(ERR_MSG_CANNOT_CREATE_ADAPTER, adapterClass.getSimpleName(), integration.getName()));
         } catch (Exception e) {
+            // todo interrupt execution since we can not create adapters for all integrations?
             LOGGER.error(e.getMessage(), e);
         }
-        return integrationAdapter;
+        // can be null?
+        return adapter;
     }
 
     private Constructor getCorrectConstructor(Class<? extends IntegrationAdapter> adapterClass) {
         return Arrays.stream(adapterClass.getDeclaredConstructors())
                      .filter(this::hasConstructorIntegrationParam)
                      .findFirst()
-                     .orElseThrow(() -> new IntegrationException("Adapter must to have at least one constructor with Integration type parameter"));
+                     .orElseThrow(() -> new IntegrationException("Adapter must have at least one constructor with Integration type parameter"));
     }
 
     private boolean hasConstructorIntegrationParam(Constructor constructor) {
@@ -189,4 +202,5 @@ public abstract class AbstractProxy {
     public String getGroup() {
         return group;
     }
+
 }
