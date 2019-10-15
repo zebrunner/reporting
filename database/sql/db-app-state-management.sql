840790e896e271a1ae8fa9f79a3b3faecd36ffb9
Nikita Sidorevich <nikitasidorevich@gmail.com>
2019-10-15 18:46:27 +0300

    state management sql script added

---
+++ database/sql/db-app-state-management.sql
@@ -0,0 +1,42 @@
+SET SCHEMA 'zafira';
+
+CREATE TABLE databasechangelog (
+  id VARHCHAR(255) NOT NULL,
+  author VARCHAR(255) NOT NULL,
+  filename VARCHAR(255) NOT NULL,
+  dateexecuted TIMESTAMP NOT NULL,
+  orderexecuted INT NOT NULL,
+  exectype VARCHAR(10) NOT NULL,
+  md5sum VARCHAR(35),
+  description VARCHAR(255),
+  comments VARCHAR(255),
+  tag VARCHAR(255),
+  liquibase VARCHAR(20),
+  contexts VARCHAR(255),
+  labels VARCHAR(255),
+  deployment_id VARCHAR(10)
+);
+
+CREATE TABLE databasechangeloglock (
+    id INT NOT NULL,
+    locked BOOLEAN NOT NULL,
+    lockgranted TIMESTAMP,
+    lockedby VARCHAR(255),
+    CONSTRAINT databasechangeloglock_pkey PRIMARY KEY (id)
+);
+
+INSERT INTO databasechangelog(
+  id,
+  author,
+  filename,
+  dateexecuted,
+  orderexecuted,
+  exectype
+) VALUES (
+  '01_create_integrations_structure',
+  'brutskov',
+  'classpath:db/changelog.yml',
+  current_timestamp,
+  1,
+  'EXECUTED'
+);
