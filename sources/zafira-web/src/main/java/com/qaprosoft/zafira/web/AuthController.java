6b16fd856fffd9faf511c690d91cb6df5fd8be8b
Bogdan Rutskov <rutkovba@gmail.com>
2019-11-04 07:30:25 -0500

    auth controller was refactored

--- sources/zafira-web/src/main/java/com/qaprosoft/zafira/web/AuthController.java
+++ sources/zafira-web/src/main/java/com/qaprosoft/zafira/web/AuthController.java
@@ -15,8 +15,6 @@
  *******************************************************************************/
 package com.qaprosoft.zafira.web;
 
-import com.qaprosoft.zafira.dbaccess.utils.TenancyContext;
-import com.qaprosoft.zafira.models.db.Invitation;
 import com.qaprosoft.zafira.models.db.User;
 import com.qaprosoft.zafira.models.dto.auth.AccessTokenType;
 import com.qaprosoft.zafira.models.dto.auth.AuthTokenType;
@@ -25,29 +23,17 @@ import com.qaprosoft.zafira.models.dto.auth.EmailType;
 import com.qaprosoft.zafira.models.dto.auth.RefreshTokenType;
 import com.qaprosoft.zafira.models.dto.auth.TenantAuth;
 import com.qaprosoft.zafira.models.dto.auth.TenantType;
-import com.qaprosoft.zafira.models.dto.user.PasswordChangingType;
 import com.qaprosoft.zafira.models.dto.user.PasswordType;
 import com.qaprosoft.zafira.models.dto.user.UserType;
-import com.qaprosoft.zafira.service.ForgotPasswordService;
-import com.qaprosoft.zafira.service.GroupService;
-import com.qaprosoft.zafira.service.InvitationService;
-import com.qaprosoft.zafira.service.JWTService;
-import com.qaprosoft.zafira.service.UserService;
+import com.qaprosoft.zafira.service.AuthService;
 import com.qaprosoft.zafira.service.exception.ForbiddenOperationException;
-import com.qaprosoft.zafira.service.exception.IllegalOperationException;
 import com.qaprosoft.zafira.service.exception.InvalidCredentialsException;
-import com.qaprosoft.zafira.service.exception.UserNotFoundException;
-import com.qaprosoft.zafira.service.integration.tool.impl.AccessManagementService;
-import com.qaprosoft.zafira.service.util.URLResolver;
 import com.qaprosoft.zafira.web.util.swagger.ApiResponseStatuses;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
-import org.apache.commons.lang3.StringUtils;
 import org.dozer.Mapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -68,69 +54,36 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
-import static com.qaprosoft.zafira.service.exception.IllegalOperationException.IllegalOperationErrorDetail.USER_CAN_NOT_BE_CREATED;
-
 @Api("Auth API")
 @CrossOrigin
 @RequestMapping(path = "api/auth", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 @RestController
 public class AuthController extends AbstractController {
 
-    private static final String ERR_MSG_USER_ALREADY_EXISTS = "User with such username already exists";
+    private final AuthService authService;
+    private final AuthenticationManager authenticationInternalManager;
+    private final AuthenticationManager authenticationLdapManager;
+    private final Mapper mapper;
 
-    @Autowired
-    private JWTService jwtService;
-
-    @Autowired
-    private ForgotPasswordService forgotPasswordService;
-
-    @Autowired
-    private UserService userService;
-
-    @Autowired
-    private AccessManagementService accessManagementService;
-
-    @Autowired
-    private GroupService groupService;
-
-    @Autowired
-    private InvitationService invitationService;
-
-    @Autowired
-    private AuthenticationManager authenticationInternalManager;
-
-    @Autowired
-    private AuthenticationManager authenticationLdapManager;
-
-    @Autowired
-    private URLResolver urlResolver;
-
-    @Autowired
-    private Mapper mapper;
-
-    @Value("${zafira.admin.username}")
-    private String adminUsername;
-
-    @Value("${zafira.multitenant}")
-    private boolean isMultitenant;
-
-    @Value("${zafira.use-artifact-proxy:false}")
-    private boolean useArtifactsProxy;
+    public AuthController(AuthService authService, AuthenticationManager authenticationInternalManager, AuthenticationManager authenticationLdapManager, Mapper mapper) {
+        this.authService = authService;
+        this.authenticationInternalManager = authenticationInternalManager;
+        this.authenticationLdapManager = authenticationLdapManager;
+        this.mapper = mapper;
+    }
 
     @ApiResponseStatuses
     @ApiOperation(value = "Get current tenant", nickname = "getTenant", httpMethod = "GET", response = String.class)
     @GetMapping("/tenant")
     public TenantType getTenant() {
-        TenantType tenantType = new TenantType(TenancyContext.getTenantName(), urlResolver.getServiceURL(), useArtifactsProxy);
-        tenantType.setMultitenant(isMultitenant);
-        return tenantType;
+        return authService.getTenantInfo();
     }
 
     @ApiResponseStatuses
     @ApiOperation(value = "Check tenant permissions", nickname = "checkPermissions", httpMethod = "POST")
     @PostMapping("/tenant/verification")
     public ResponseEntity<Void> checkPermissions(@Valid @RequestBody TenantAuth tenantAuth) {
-        boolean result = jwtService.checkPermissions(tenantAuth.getTenantName(), tenantAuth.getToken(), tenantAuth.getPermissions());
+        boolean result = authService.checkTenantPermissions(tenantAuth);
         HttpStatus httpStatus = result ? HttpStatus.OK : HttpStatus.FORBIDDEN;
         return new ResponseEntity<>(httpStatus);
     }
@@ -142,7 +95,7 @@ public class AuthController extends AbstractController {
         AuthTokenType authToken;
         try {
             Authentication authentication;
-            User user = userService.getUserByUsernameOrEmail(credentials.getUsername());
+            User user = authService.getUser(credentials.getUsername());
 
             final AuthenticationManager authenticationManager = user == null
                     || user.getSource().equals(User.Source.LDAP) ? authenticationLdapManager : authenticationInternalManager;
@@ -150,16 +103,9 @@ public class AuthController extends AbstractController {
             authentication = authenticationManager
                     .authenticate(new UsernamePasswordAuthenticationToken(credentials.getUsername(), credentials.getPassword()));
 
-            user = userService.getUserByUsernameOrEmail(credentials.getUsername());
+            authToken = authService.buildAuthToken(credentials.getUsername());
 
             SecurityContextHolder.getContext().setAuthentication(authentication);
-
-            final String tenant = TenancyContext.getTenantName();
-
-            authToken = new AuthTokenType("Bearer", jwtService.generateAuthToken(user, tenant),
-                    jwtService.generateRefreshToken(user, tenant), jwtService.getExpiration(), tenant);
-
-            userService.updateLastLoginDate(user.getId());
         } catch (Exception e) {
             throw new BadCredentialsException(e.getMessage());
         }
@@ -170,24 +116,8 @@ public class AuthController extends AbstractController {
     @ApiOperation(value = "Sign up", nickname = "signup", httpMethod = "POST")
     @PostMapping("/signup")
     public void signup(@RequestHeader("Access-Token") String token, @Valid @RequestBody UserType userType) {
-        if (userService.getUserByUsername(userType.getUsername()) != null) {
-            throw new IllegalOperationException(USER_CAN_NOT_BE_CREATED, ERR_MSG_USER_ALREADY_EXISTS);
-        }
-        Invitation invitation = invitationService.getInvitationByToken(token);
-        if (invitation == null) {
-            throw new ForbiddenOperationException();
-        }
-        if (invitation.getSource().equals(User.Source.LDAP)) {
-            boolean ldapEnabled = accessManagementService.isEnabledAndConnected(null);
-            boolean userExists = accessManagementService.isUserExists(userType.getUsername());
-            if (ldapEnabled && !userExists) {
-                throw new ForbiddenOperationException();
-            }
-        }
-        userType.setSource(invitation.getSource());
-        invitation.setStatus(Invitation.Status.ACCEPTED);
-        invitationService.updateInvitation(invitation);
-        userService.createOrUpdateUser(mapper.map(userType, User.class), groupService.getGroupById(invitation.getGroupId()));
+        User user = mapper.map(userType, User.class);
+        authService.registerUser(user, token);
     }
 
     @ApiResponseStatuses
@@ -195,73 +125,34 @@ public class AuthController extends AbstractController {
     @PostMapping("/refresh")
     public AuthTokenType refresh(@RequestBody @Valid RefreshTokenType refreshToken)
             throws BadCredentialsException, ForbiddenOperationException {
-        AuthTokenType authToken;
+        AuthTokenType authTokenType;
         try {
-            User jwtUser = jwtService.parseRefreshToken(refreshToken.getRefreshToken());
-
-            User user = userService.getUserById(jwtUser.getId());
-            if (user == null) {
-                throw new UserNotFoundException();
-            }
-
-            if (User.Status.INACTIVE.equals(user.getStatus())) {
-                throw new BadCredentialsException("Unable to refresh auth token");
-            }
-
-            if (!TenancyContext.getTenantName().equals(jwtUser.getTenant())) {
-                throw new InvalidCredentialsException("Invalid tenant");
-            }
-
-            // TODO: Do not verify password for demo user as far as it breaks demo JWT token
-            if (!StringUtils.equals(adminUsername, user.getUsername()) && !StringUtils.equals(user.getPassword(), jwtUser.getPassword())) {
-                throw new InvalidCredentialsException();
-            }
-
-            final String tenant = TenancyContext.getTenantName();
-
-            authToken = new AuthTokenType("Bearer", jwtService.generateAuthToken(user, tenant),
-                    jwtService.generateRefreshToken(user, tenant), jwtService.getExpiration(),
-                    TenancyContext.getTenantName());
-
-            userService.updateLastLoginDate(user.getId());
-        } catch (Exception e) {
-            throw new ForbiddenOperationException(e);
+            authTokenType = authService.refreshAuthToken(refreshToken.getRefreshToken());
+        } catch (InvalidCredentialsException e) {
+            throw new BadCredentialsException(e.getMessage(), e);
         }
-        return authToken;
+        return authTokenType;
     }
 
     @ApiResponseStatuses
     @ApiOperation(value = "Forgot password", nickname = "forgotPassword", httpMethod = "POST")
     @PostMapping("/password/forgot")
     public void forgotPassword(@Valid @RequestBody EmailType emailType) {
-        User user = userService.getUserByEmail(emailType.getEmail());
-        if (user != null) {
-            forgotPasswordService.sendForgotPasswordEmail(emailType, user);
-        }
+        authService.sendForgotPasswordEmail(emailType);
     }
 
     @ApiResponseStatuses
     @ApiOperation(value = "Get forgot password type by token", nickname = "getForgotPasswordType", httpMethod = "GET")
     @GetMapping("/password/forgot")
     public void getForgotPasswordType(@RequestParam("token") String token) throws ForbiddenOperationException {
-        User user = userService.getUserByResetToken(token);
-        if (user == null || !user.getSource().equals(User.Source.INTERNAL)) {
-            throw new ForbiddenOperationException();
-        }
+        authService.getUserByResetToken(token);
     }
 
     @ApiResponseStatuses
     @ApiOperation(value = "Reset password", nickname = "resetPassword", httpMethod = "PUT")
     @PutMapping("/password")
     public void resetPassword(@RequestHeader("Access-Token") String token, @Valid @RequestBody PasswordType passwordType) {
-        User user = userService.getUserByResetToken(token);
-        if (user != null && user.getSource().equals(User.Source.INTERNAL)) {
-            PasswordChangingType passwordChangingType = new PasswordChangingType();
-            passwordChangingType.setPassword(passwordType.getPassword());
-            passwordChangingType.setUserId(user.getId());
-            userService.updateUserPassword(null, passwordChangingType);
-            userService.updateResetToken(null, user.getId());
-        }
+        authService.resetPassword(token, passwordType.getPassword());
     }
 
     @ApiResponseStatuses
@@ -269,7 +160,7 @@ public class AuthController extends AbstractController {
     @ApiImplicitParams({ @ApiImplicitParam(name = "Authorization", paramType = "header") })
     @GetMapping("/access")
     public AccessTokenType accessToken() {
-        String token = jwtService.generateAccessToken(userService.getNotNullUserById(getPrincipalId()), TenancyContext.getTenantName());
+        String token = authService.generateAccessToken(getPrincipalId());
         return new AccessTokenType(token);
     }
 
