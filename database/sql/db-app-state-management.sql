0904d6414b83f4b58dab36eb880dba124dfc0a67
Bogdan Rutskov <rutkovba@gmail.com>
2019-11-21 06:53:16 -0500

    added alter script to initial liquibase script

--- database/sql/db-app-state-management.sql
+++ database/sql/db-app-state-management.sql
@@ -65,4 +65,12 @@ INSERT INTO databasechangelog(
      current_timestamp,
      1,
      'EXECUTED'
+ ),
+ (
+     '006_add_project_to_test_cases_unique_key',
+     'brutskov',
+     'classpath:db/changelog.yml',
+     current_timestamp,
+     1,
+     'EXECUTED'
  );
