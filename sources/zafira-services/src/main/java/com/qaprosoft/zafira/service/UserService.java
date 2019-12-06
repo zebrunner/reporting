e7bc3e7938ed9155104f60385af3cbcb5051fa63
Bogdan Rutskov <rutkovba@gmail.com>
2019-12-06 08:51:23 -0500

    made possible to update first and last names only on profile update

--- sources/zafira-services/src/main/java/com/qaprosoft/zafira/service/UserService.java
+++ sources/zafira-services/src/main/java/com/qaprosoft/zafira/service/UserService.java
@@ -193,8 +193,11 @@ public class UserService implements TenancyDbInitial {
     @CacheEvict(value = "users", condition = "#user.id != null", key = "new com.qaprosoft.zafira.dbaccess.utils.TenancyContext().getTenantName() + ':' + #user.id")
     @Transactional(rollbackFor = Exception.class)
     public User updateUser(User user) {
-        userMapper.updateUser(user);
-        return user;
+        User dbUser = getNotNullUserById(user.getId());
+        dbUser.setFirstName(user.getFirstName());
+        dbUser.setLastName(user.getLastName());
+        userMapper.updateUser(dbUser);
+        return dbUser;
     }
 
     @Transactional(rollbackFor = Exception.class)
