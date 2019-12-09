17187ed205d2bff509eda6bfdb764c457fbdad6e
Volodymyr Martseniuk <vmartseniuk@solvd.com>
2019-12-09 10:16:08 +0200

    number fix

--- database/sql/db-mng-data.sql
+++ database/sql/db-mng-data.sql
@@ -1177,9 +1177,9 @@ SELECT
       axisLabel : {
         formatter: (value) => {
           if(value == 0) return value
-          if(value >= 1000000000) return `${value/1000000}B`
-          else if(value >= 1000000) return `${value/1000000}M`
-          else if (value >= 1000) return `${value/1000}K`
+          if(value >= 1000000000) return `${(value/1000000).toFixed(2)}B`
+          else if(value >= 1000000) return `${(value/1000000).toFixed(2)}M`
+          else if (value >= 1000) return `${(value/1000).toFixed(2)}K`
           else return value
         }
       }
