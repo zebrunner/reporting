68bf35df81027ad9a2fd2b5cc55b52927706197d
tsvrko <tsvrko@gmail.com>
2020-02-13 17:19:29 +0300

    User id is selected instead of the whole row

--- sources/zafira-dbaccess/src/main/resources/db/changeset/015_add_default_runs_view_preference.sql
+++ sources/zafira-dbaccess/src/main/resources/db/changeset/015_add_default_runs_view_preference.sql
@@ -3,7 +3,7 @@ $$
     DECLARE
         user_id_var users.id%TYPE;
     BEGIN
-        FOR user_id_var IN SELECT * FROM users
+        FOR user_id_var IN SELECT id FROM users
             LOOP
                 INSERT INTO user_preferences (name, value, user_id) VALUES ('DEFAULT_TEST_VIEW', 'runs', user_id_var);
             END LOOP;
