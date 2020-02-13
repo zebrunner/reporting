f75fe4f4ca56ac6e24145ebaefb90f3e66289d77
tsvrko <tsvrko@gmail.com>
2020-02-13 16:41:10 +0300

    Single preference update is added

--- sources/zafira-web/src/main/java/com/qaprosoft/zafira/web/documented/UserDocumentedController.java
+++ sources/zafira-web/src/main/java/com/qaprosoft/zafira/web/documented/UserDocumentedController.java
@@ -19,6 +19,7 @@ import com.qaprosoft.zafira.dbaccess.dao.mysql.application.search.SearchResult;
 import com.qaprosoft.zafira.dbaccess.dao.mysql.application.search.UserSearchCriteria;
 import com.qaprosoft.zafira.models.db.User;
 import com.qaprosoft.zafira.models.db.UserPreference;
+import com.qaprosoft.zafira.models.dto.UserPreferenceDTO;
 import com.qaprosoft.zafira.models.dto.errors.ErrorResponse;
 import com.qaprosoft.zafira.models.dto.user.ChangePasswordDTO;
 import com.qaprosoft.zafira.models.dto.user.UserType;
@@ -230,6 +231,24 @@ public interface UserDocumentedController {
             @ApiResponse(code = 200, message = "Returns attached preferences", response = List.class)
     })
     List<UserPreference> createUserPreference(long userId, List<UserPreference> preferences);
+
+    @ApiOperation(
+            value = "Updates single user preference",
+            notes = "Returns updated integration",
+            nickname = "createUserPreference",
+            httpMethod = "PUT",
+            response = UserPreferenceDTO.class
+    )
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", paramType = "header", required = true, value = "The auth token (Bearer)"),
+            @ApiImplicitParam(name = "id", paramType = "path", dataType = "number", required = true, value = "The path reference id"),
+            @ApiImplicitParam(name = "name", paramType = "query", dataType = "string", required = true, value = "User preference name"),
+            @ApiImplicitParam(name = "value", paramType = "query", dataType = "string", required = true, value = "User preference value")
+    })
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Returns the updated user preference", response = UserPreferenceDTO.class)
+    })
+    UserPreferenceDTO createUserPreference(long userId, UserPreference.Name name, String value);
 
     @ApiOperation(
             value = "Resets current user preferences to default",
