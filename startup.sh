3f21adb0263edfa8e252fd7977d8edf9e7ad53ee
tsvrko <tsvrko@gmail.com>
2020-03-02 16:51:17 +0300

    DEBUG_SUSPEND_STARTUP is added

--- startup.sh
+++ startup.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-
-if [ "${DEBUG_ENABLED}" = true ] ; then
-  java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:${DEBUG_PORT} -jar /app/zafira-service.jar
-else
-  java -jar /app/zafira-service.jar
+DEBUG_SUSPEND_OPTION="n"
+if [ "${DEBUG_SUSPEND_STARTUP}" = true ]; then
+  DEBUG_SUSPEND_OPTION="y"
 fi
+
+DEBUG_PORT_VALUE="5005"
+if [ -n "${DEBUG_PORT}" ]; then
+  DEBUG_PORT_VALUE="${DEBUG_PORT}"
+fi
+
+JAVA_DEBUG_OPTS=""
+if [ "${DEBUG_ENABLED}" = true ]; then
+  JAVA_DEBUG_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=$DEBUG_SUSPEND_OPTION,address=*:$DEBUG_PORT_VALUE"
+fi
+
+JAVA_EXEC_COMMAND="$JAVA_DEBUG_OPTS -jar /app/zafira-service.jar"
+
+java $JAVA_EXEC_COMMAND
