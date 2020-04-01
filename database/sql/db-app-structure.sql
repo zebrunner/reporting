8ce66efc562cb27ab7d93113b627ffbc383231b7
brutskov <rutkovba@gmail.com>
2020-04-01 10:24:23 +0300

    user launchers table was added

--- database/sql/db-app-structure.sql
+++ database/sql/db-app-structure.sql
@@ -874,6 +874,24 @@ CREATE UNIQUE INDEX LAUNCHER_CALLBACK_REFERENCE_UNIQUE ON launcher_callbacks (re
 CREATE TRIGGER update_timestamp_launcher_callback BEFORE INSERT OR UPDATE ON launcher_callbacks FOR EACH ROW EXECUTE PROCEDURE update_timestamp();
 
 
+CREATE TABLE IF NOT EXISTS user_launcher_preferences (
+    id SERIAL,
+    user_id INT NOT NULL,
+    launcher_id INT NOT NULL,
+    favorite BOOLEAN NULL DEFAULT FALSE,
+    modified_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
+    created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+        ON DELETE CASCADE
+        ON UPDATE NO ACTION,
+    FOREIGN KEY (launcher_id) REFERENCES launchers (id)
+        ON DELETE CASCADE
+        ON UPDATE NO ACTION);
+CREATE UNIQUE INDEX USER_LAUNCHER_PREFERENCES_USER_ID_LAUNCHER_ID_UNIQUE ON user_launcher_preferences (user_id, launcher_id);
+CREATE TRIGGER update_timestamp_user_launcher_preferences BEFORE INSERT OR UPDATE ON user_launcher_preferences FOR EACH ROW EXECUTE PROCEDURE update_timestamp();
+
+
 DROP TABLE IF EXISTS test_sessions;
 CREATE TABLE test_sessions (
    id SERIAL,
