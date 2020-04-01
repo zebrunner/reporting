50cdb3aea37ebac157f8ef9c8af4736a3968a9bf
brutskov <rutkovba@gmail.com>
2020-04-01 13:23:27 +0300

    got rid of unused timestamp columns

--- database/sql/db-app-structure.sql
+++ database/sql/db-app-structure.sql
@@ -879,8 +879,6 @@ CREATE TABLE IF NOT EXISTS user_launcher_preferences (
     user_id INT NOT NULL,
     launcher_id INT NOT NULL,
     favorite BOOLEAN NULL DEFAULT FALSE,
-    modified_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
-    created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users (id)
         ON DELETE CASCADE
@@ -889,7 +887,6 @@ CREATE TABLE IF NOT EXISTS user_launcher_preferences (
         ON DELETE CASCADE
         ON UPDATE NO ACTION);
 CREATE UNIQUE INDEX USER_LAUNCHER_PREFERENCES_USER_ID_LAUNCHER_ID_UNIQUE ON user_launcher_preferences (user_id, launcher_id);
-CREATE TRIGGER update_timestamp_user_launcher_preferences BEFORE INSERT OR UPDATE ON user_launcher_preferences FOR EACH ROW EXECUTE PROCEDURE update_timestamp();
 
 
 DROP TABLE IF EXISTS test_sessions;
