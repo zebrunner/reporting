ef3c595b3082db9ac9cd62401219ef1f0c919a72
Nikita Sidorevich <nikitasidorevich@gmail.com>
2020-07-17 16:51:53 +0300

    Configuration adjusted

--- zebrunner-server.sh
+++ zebrunner-server.sh
@@ -8,11 +8,6 @@ case "$1" in
         echo "Starting Zebrunner..."
         docker network inspect infra >/dev/null 2>&1 || docker network create infra
         docker-compose pull && docker-compose up -d
-        for i in {0..8}; do
-        echo "Starting Zebrunner..."
-        sleep 20s
-        done
-        docker restart logstash >/dev/null 2>&1
         ;;
     stop)
         echo "Stopping Zebrunner..."
