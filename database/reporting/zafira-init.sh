2fa8b4d72084c245f0394ba4302384d7b214f476
vdelendik <vdelendik@zebrunner.com>
2020-08-19 14:04:47 +0000

    reporting-jenkins integration setup

--- database/reporting/zafira-init.sh
+++ database/reporting/zafira-init.sh
@@ -14,41 +14,9 @@ psql -v ON_ERROR_STOP=1 --username $POSTGRES_USER -f /docker-entrypoint-initdb.d
 psql -v ON_ERROR_STOP=1 --username $POSTGRES_USER -f /docker-entrypoint-initdb.d/sql/db-views-cron.sql
 psql -v ON_ERROR_STOP=1 --username $POSTGRES_USER -f /docker-entrypoint-initdb.d/sql/db-widgets.sql
 
-if [ "$ZAFIRA_AMAZON_ENABLED" == true ];
+if [[ -f /docker-entrypoint-initdb.d/sql/db-jenkins-integration.sql ]];
 then
-    psql --username $POSTGRES_USER -c "update zafira.integrations set enabled='true' where zafira.integrations.name='AMAZON';"
-    psql --username $POSTGRES_USER -c "update zafira.integration_settings set value='$ZAFIRA_AMAZON_BUCKET' from zafira.integration_params where integration_settings.integration_param_id=integration_params.id and integration_params.name='AMAZON_BUCKET';"
-    psql --username $POSTGRES_USER -c "update zafira.integration_settings set value='$ZAFIRA_AMAZON_ACCESS_KEY' from zafira.integration_params where integration_settings.integration_param_id=integration_params.id and integration_params.name='AMAZON_ACCESS_KEY';"
-    psql --username $POSTGRES_USER -c "update zafira.integration_settings set value='$ZAFIRA_AMAZON_SECRET_KEY' from zafira.integration_params where integration_settings.integration_param_id=integration_params.id and integration_params.name='AMAZON_SECRET_KEY';"
-    psql --username $POSTGRES_USER -c "update zafira.integration_settings set value='$ZAFIRA_AMAZON_REGION' from zafira.integration_params where integration_settings.integration_param_id=integration_params.id and integration_params.name='AMAZON_REGION';"
-fi
-
-if [ "$ZAFIRA_JENKINS_ENABLED" == true ];
-then
-    psql --username $POSTGRES_USER -c "update zafira.integrations set enabled='true' where zafira.integrations.name='JENKINS';"
-    psql --username $POSTGRES_USER -c "update zafira.integration_settings set value='$ZAFIRA_JENKINS_URL' from zafira.integration_params where integration_settings.integration_param_id=integration_params.id and integration_params.name='JENKINS_URL';"
-    psql --username $POSTGRES_USER -c "update zafira.integration_settings set value='$ZAFIRA_JENKINS_USER' from zafira.integration_params where integration_settings.integration_param_id=integration_params.id and integration_params.name='JENKINS_USER';"
-    psql --username $POSTGRES_USER -c "update zafira.integration_settings set value='$ZAFIRA_JENKINS_API_TOKEN_OR_PASSWORD' from zafira.integration_params where integration_settings.integration_param_id=integration_params.id and integration_params.name='JENKINS_API_TOKEN_OR_PASSWORD';"
-    psql --username $POSTGRES_USER -c "update zafira.integration_settings set value='$ZAFIRA_JENKINS_FOLDER' from zafira.integration_params where integration_settings.integration_param_id=integration_params.id and integration_params.name='JENKINS_FOLDER';"
-fi
-
-if [ "$ZAFIRA_EMAIL_ENABLED" == true ];
-then
-    psql --username $POSTGRES_USER -c "update zafira.integrations set enabled='true' where zafira.integrations.name='EMAIL';"
-    psql --username $POSTGRES_USER -c "update zafira.integration_settings set value='$ZAFIRA_EMAIL_HOST' from zafira.integration_params where integration_settings.integration_param_id=integration_params.id and integration_params.name='EMAIL_HOST';"
-    psql --username $POSTGRES_USER -c "update zafira.integration_settings set value='$ZAFIRA_EMAIL_PORT' from zafira.integration_params where integration_settings.integration_param_id=integration_params.id and integration_params.name='EMAIL_PORT';"
-    psql --username $POSTGRES_USER -c "update zafira.integration_settings set value='$ZAFIRA_EMAIL_USER' from zafira.integration_params where integration_settings.integration_param_id=integration_params.id and integration_params.name='EMAIL_USER';"
-    psql --username $POSTGRES_USER -c "update zafira.integration_settings set value='$ZAFIRA_EMAIL_PASSWORD' from zafira.integration_params where integration_settings.integration_param_id=integration_params.id and integration_params.name='EMAIL_PASSWORD';"
-    psql --username $POSTGRES_USER -c "update zafira.integration_settings set value='$ZAFIRA_EMAIL_FROM_ADDRESS' from zafira.integration_params where integration_settings.integration_param_id=integration_params.id and integration_params.name='EMAIL_FROM_ADDRESS';"
-fi
-
-if [ "$RABBITMQ_ENABLED" == true ];
-then
-    psql --username $POSTGRES_USER -c "update zafira.integrations set enabled='true' where zafira.integrations.name='RABBITMQ';"
-    psql --username $POSTGRES_USER -c "update zafira.integration_settings set value='$RABBITMQ_HOST' from zafira.integration_params where integration_settings.integration_param_id=integration_params.id and integration_params.name='RABBITMQ_HOST';"
-    psql --username $POSTGRES_USER -c "update zafira.integration_settings set value='$RABBITMQ_PORT' from zafira.integration_params where integration_settings.integration_param_id=integration_params.id and integration_params.name='RABBITMQ_PORT';"
-    psql --username $POSTGRES_USER -c "update zafira.integration_settings set value='$RABBITMQ_USERNAME' from zafira.integration_params where integration_settings.integration_param_id=integration_params.id and integration_params.name='RABBITMQ_USER';"
-    psql --username $POSTGRES_USER -c "update zafira.integration_settings set value='$RABBITMQ_PASSWORD' from zafira.integration_params where integration_settings.integration_param_id=integration_params.id and integration_params.name='RABBITMQ_PASSWORD';"
+  psql -v ON_ERROR_STOP=1 --username $POSTGRES_USER -f /docker-entrypoint-initdb.d/sql/db-jenkins-integration.sql
 fi
 
 if [ "$ZEBRUNNER_ENABLED" == true ];
