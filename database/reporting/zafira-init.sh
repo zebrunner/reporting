0a0738ff3e2142a3e486e7e408b56cf972457eb7
vdelendik <vdelendik@zebrunner.com>
2020-08-19 14:40:15 +0000

    added selenoid integration and simplified jenkins and mcloud

--- database/reporting/zafira-init.sh
+++ database/reporting/zafira-init.sh
@@ -24,43 +24,7 @@ then
   psql -v ON_ERROR_STOP=1 --username $POSTGRES_USER -f /docker-entrypoint-initdb.d/sql/db-mcloud-integration.sql
 fi
 
-
-if [ "$ZEBRUNNER_ENABLED" == true ];
+if [[ -f /docker-entrypoint-initdb.d/sql/db-selenium-integration.sql ]];
 then
-    psql --username $POSTGRES_USER -c "update zafira.integrations set enabled='true' where zafira.integrations.name='ZEBRUNNER';"
-    psql --username $POSTGRES_USER -c "update zafira.integration_settings set value='$ZEBRUNNER_URL' from zafira.integration_params where integration_settings.integration_param_id=integration_params.id and integration_params.name='ZEBRUNNER_URL';"
-    psql --username $POSTGRES_USER -c "update zafira.integration_settings set value='$ZEBRUNNER_USER' from zafira.integration_params where integration_settings.integration_param_id=integration_params.id and integration_params.name='ZEBRUNNER_USER';"
-    psql --username $POSTGRES_USER -c "update zafira.integration_settings set value='$ZEBRUNNER_PASSWORD' from zafira.integration_params where integration_settings.integration_param_id=integration_params.id and integration_params.name='ZEBRUNNER_PASSWORD';"
-fi
-
-if [ "$SELENIUM_ENABLED" == true ];
-then
-    psql --username $POSTGRES_USER -c "update zafira.integrations set enabled='true' where zafira.integrations.name='SELENIUM';"
-    psql --username $POSTGRES_USER -c "update zafira.integration_settings set value='$SELENIUM_URL' from zafira.integration_params where integration_settings.integration_param_id=integration_params.id and integration_params.name='SELENIUM_URL';"
-    psql --username $POSTGRES_USER -c "update zafira.integration_settings set value='$SELENIUM_USER' from zafira.integration_params where integration_settings.integration_param_id=integration_params.id and integration_params.name='SELENIUM_USER';"
-    psql --username $POSTGRES_USER -c "update zafira.integration_settings set value='$SELENIUM_PASSWORD' from zafira.integration_params where integration_settings.integration_param_id=integration_params.id and integration_params.name='SELENIUM_PASSWORD';"
-fi
-
-if [ "$AEROKUBE_ENABLED" == true ];
-then
-    psql --username $POSTGRES_USER -c "update zafira.integrations set enabled='true' where zafira.integrations.name='AEROKUBE';"
-    psql --username $POSTGRES_USER -c "update zafira.integration_settings set value='$AEROKUBE_URL' from zafira.integration_params where integration_settings.integration_param_id=integration_params.id and integration_params.name='AEROKUBE_URL';"
-    psql --username $POSTGRES_USER -c "update zafira.integration_settings set value='$AEROKUBE_USER' from zafira.integration_params where integration_settings.integration_param_id=integration_params.id and integration_params.name='AEROKUBE_USER';"
-    psql --username $POSTGRES_USER -c "update zafira.integration_settings set value='$AEROKUBE_PASSWORD' from zafira.integration_params where integration_settings.integration_param_id=integration_params.id and integration_params.name='AEROKUBE_PASSWORD';"
-fi
-
-if [ "$BROWSERSTACK_ENABLED" == true ];
-then
-    psql --username $POSTGRES_USER -c "update zafira.integrations set enabled='true' where zafira.integrations.name='BROWSERSTACK';"
-    psql --username $POSTGRES_USER -c "update zafira.integration_settings set value='$BROWSERSTACK_URL' from zafira.integration_params where integration_settings.integration_param_id=integration_params.id and integration_params.name='BROWSERSTACK_URL';"
-    psql --username $POSTGRES_USER -c "update zafira.integration_settings set value='$BROWSERSTACK_USER' from zafira.integration_params where integration_settings.integration_param_id=integration_params.id and integration_params.name='BROWSERSTACK_USER';"
-    psql --username $POSTGRES_USER -c "update zafira.integration_settings set value='$BROWSERSTACK_PASSWORD' from zafira.integration_params where integration_settings.integration_param_id=integration_params.id and integration_params.name='BROWSERSTACK_ACCESS_KEY';"
-fi
-
-if [ "$SAUCELABS_ENABLED" == true ];
-then
-    psql --username $POSTGRES_USER -c "update zafira.integrations set enabled='true' where zafira.integrations.name='SAUCELABS';"
-    psql --username $POSTGRES_USER -c "update zafira.integration_settings set value='$SAUCELABS_URL' from zafira.integration_params where integration_settings.integration_param_id=integration_params.id and integration_params.name='SAUCELABS_URL';"
-    psql --username $POSTGRES_USER -c "update zafira.integration_settings set value='$SAUCELABS_USER' from zafira.integration_params where integration_settings.integration_param_id=integration_params.id and integration_params.name='SAUCELABS_USER';"
-    psql --username $POSTGRES_USER -c "update zafira.integration_settings set value='$SAUCELABS_PASSWORD' from zafira.integration_params where integration_settings.integration_param_id=integration_params.id and integration_params.name='SAUCELABS_PASSWORD';"
+  psql -v ON_ERROR_STOP=1 --username $POSTGRES_USER -f /docker-entrypoint-initdb.d/sql/db-selenium-integration.sql
 fi
