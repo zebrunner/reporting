64673359d66f4ee42e189b460fceda62ff3d34d3
vdelendik <vdelendik@zebrunner.com>
2021-06-13 12:55:27 +0000

    supported setup for macOS

--- zebrunner.sh
+++ zebrunner.sh
@@ -2,16 +2,16 @@
 
   setup() {
     # PREREQUISITES: valid values inside ZBR_* env vars!
-    local url="$ZBR_PROTOCOL://$ZBR_HOSTNAME:$ZBR_PORT"
+    local url="${ZBR_PROTOCOL}://${ZBR_HOSTNAME}:${ZBR_PORT}"
 
     cp configuration/_common/hosts.env.original configuration/_common/hosts.env
-    sed -i "s#http://localhost:8081#${url}#g" configuration/_common/hosts.env
+    replace configuration/_common/hosts.env "http://localhost:8081" "${url}"
 
     cp configuration/reporting-service/variables.env.original configuration/reporting-service/variables.env
-    sed -i "s#http://localhost:8081#${url}#g" configuration/reporting-service/variables.env
+    replace configuration/reporting-service/variables.env "http://localhost:8081" "${url}"
 
     cp configuration/reporting-ui/variables.env.original configuration/reporting-ui/variables.env
-    sed -i "s#http://localhost:8081#${url}#g" configuration/reporting-ui/variables.env
+    replace configuration/reporting-ui/variables.env "http://localhost:8081" "${url}"
 
     cp configuration/_common/secrets.env.original configuration/_common/secrets.env
     replace configuration/_common/secrets.env "TOKEN_SIGNING_SECRET=AUwMLdWFBtUHVgvjFfMmAEadXqZ6HA4dKCiCmjgCXxaZ4ZO8od" "TOKEN_SIGNING_SECRET=${ZBR_TOKEN_SIGNING_SECRET}"
@@ -20,10 +20,10 @@
     cp configuration/_common/s3.env.original configuration/_common/s3.env
     if [[ $ZBR_MINIO_ENABLED -eq 0 ]]; then
       # use case with AWS S3
-      sed -i "s#S3_REGION=us-east-1#S3_REGION=${ZBR_STORAGE_REGION}#g" configuration/_common/s3.env
-      sed -i "s#S3_ENDPOINT=http://minio:9000#S3_ENDPOINT=${ZBR_STORAGE_ENDPOINT_PROTOCOL}://${ZBR_STORAGE_ENDPOINT_HOST}#g" configuration/_common/s3.env
-      sed -i "s#S3_BUCKET=zebrunner#S3_BUCKET=${ZBR_STORAGE_BUCKET}#g" configuration/_common/s3.env
-      sed -i "s#S3_ACCESS_KEY_ID=zebrunner#S3_ACCESS_KEY_ID=${ZBR_STORAGE_ACCESS_KEY}#g" configuration/_common/s3.env
+      replace configuration/_common/s3.env "S3_REGION=us-east-1" "S3_REGION=${ZBR_STORAGE_REGION}"
+      replace configuration/_common/s3.env "S3_ENDPOINT=http://minio:9000" "S3_ENDPOINT=${ZBR_STORAGE_ENDPOINT_PROTOCOL}://${ZBR_STORAGE_ENDPOINT_HOST}"
+      replace configuration/_common/s3.env "S3_BUCKET=zebrunner" "S3_BUCKET=${ZBR_STORAGE_BUCKET}"
+      replace configuration/_common/s3.env "S3_ACCESS_KEY_ID=zebrunner" "S3_ACCESS_KEY_ID=${ZBR_STORAGE_ACCESS_KEY}"
       replace configuration/_common/s3.env "S3_SECRET=J33dNyeTDj" "S3_SECRET=${ZBR_STORAGE_SECRET_KEY}"
     fi
 
@@ -31,8 +31,8 @@
     if [[ $ZBR_MINIO_ENABLED -eq 0 ]]; then
       # use case with AWS S3
       replace configuration/zebrunner-proxy/nginx.conf "custom_secret_value" "${ZBR_STORAGE_AGENT_KEY}"
-      sed -i "s#/zebrunner/#/${ZBR_STORAGE_BUCKET}/#g" configuration/zebrunner-proxy/nginx.conf
-      sed -i "s#http://minio:9000#${ZBR_STORAGE_ENDPOINT_PROTOCOL}://${ZBR_STORAGE_ENDPOINT_HOST}#g" configuration/zebrunner-proxy/nginx.conf
+      replace configuration/zebrunner-proxy/nginx.conf "/zebrunner/" "/${ZBR_STORAGE_BUCKET}/"
+      replace configuration/zebrunner-proxy/nginx.conf "http://minio:9000" "${ZBR_STORAGE_ENDPOINT_PROTOCOL}://${ZBR_STORAGE_ENDPOINT_HOST}"
     fi
 
     cp configuration/iam-service/variables.env.original configuration/iam-service/variables.env
@@ -46,21 +46,21 @@
     replace configuration/reporting-service/variables.env "DATABASE_PASSWORD=db-changeit" "DATABASE_PASSWORD=${ZBR_POSTGRES_PASSWORD}"
 
     cp configuration/mail-service/variables.env.original configuration/mail-service/variables.env
-    sed -i "s#MAILING_HOST=smtp.gmail.com#MAILING_HOST=${ZBR_SMTP_HOST}#g" configuration/mail-service/variables.env
-    sed -i "s#MAILING_PORT=587#MAILING_PORT=${ZBR_SMTP_PORT}#g" configuration/mail-service/variables.env
-    sed -i "s#MAILING_SENDER_EMAIL=changeit#MAILING_SENDER_EMAIL=${ZBR_SMTP_EMAIL}#g" configuration/mail-service/variables.env
-    sed -i "s#MAILING_SENDER_NAME=changeit#MAILING_SENDER_NAME=${ZBR_SMTP_USER}#g" configuration/mail-service/variables.env
-    sed -i "s#MAILING_USERNAME=changeit#MAILING_USERNAME=${ZBR_SMTP_USER}#g" configuration/mail-service/variables.env
+    replace configuration/mail-service/variables.env "MAILING_HOST=smtp.gmail.com" "MAILING_HOST=${ZBR_SMTP_HOST}"
+    replace configuration/mail-service/variables.env "MAILING_PORT=587" "MAILING_PORT=${ZBR_SMTP_PORT}"
+    replace configuration/mail-service/variables.env "MAILING_SENDER_EMAIL=changeit" "MAILING_SENDER_EMAIL=${ZBR_SMTP_EMAIL}"
+    replace configuration/mail-service/variables.env "MAILING_SENDER_NAME=changeit" "MAILING_SENDER_NAME=${ZBR_SMTP_USER}"
+    replace configuration/mail-service/variables.env "MAILING_USERNAME=changeit" "MAILING_USERNAME=${ZBR_SMTP_USER}"
     replace configuration/mail-service/variables.env "MAILING_PASSWORD=changeit" "MAILING_PASSWORD=${ZBR_SMTP_PASSWORD}"
 
     cp configuration/rabbitmq/variables.env.original configuration/rabbitmq/variables.env
-    sed -i "s#RABBITMQ_DEFAULT_USER=rabbitmq-user#RABBITMQ_DEFAULT_USER=${ZBR_RABBITMQ_USER}#g" configuration/rabbitmq/variables.env
+    replace configuration/rabbitmq/variables.env "RABBITMQ_DEFAULT_USER=rabbitmq-user" "RABBITMQ_DEFAULT_USER=${ZBR_RABBITMQ_USER}"
     replace configuration/rabbitmq/variables.env "RABBITMQ_DEFAULT_PASS=rabbitmq-password" "RABBITMQ_DEFAULT_PASS=${ZBR_RABBITMQ_PASSWORD}"
     cp configuration/logstash/logstash.conf.original configuration/logstash/logstash.conf
-    sed -i "s#rabbitmq-user#${ZBR_RABBITMQ_USER}#g" configuration/logstash/logstash.conf
+    replace configuration/logstash/logstash.conf "rabbitmq-user" "${ZBR_RABBITMQ_USER}"
     replace configuration/logstash/logstash.conf "rabbitmq-password" "${ZBR_RABBITMQ_PASSWORD}"
     cp configuration/_common/rabbitmq.env.original configuration/_common/rabbitmq.env
-    sed -i "s#rabbitmq-user#${ZBR_RABBITMQ_USER}#g" configuration/_common/rabbitmq.env
+    replace configuration/_common/rabbitmq.env "rabbitmq-user" "${ZBR_RABBITMQ_USER}"
     replace configuration/_common/rabbitmq.env "rabbitmq-password" "${ZBR_RABBITMQ_PASSWORD}"
     cp configuration/rabbitmq/001-general-definition.json.original configuration/rabbitmq/definitions/001-general-definition.json
     sed -i "s#rabbitmq-user#${ZBR_RABBITMQ_USER}#g" configuration/rabbitmq/definitions/001-general-definition.json
@@ -213,10 +213,10 @@
     cp configuration/reporting-service/variables.env configuration/reporting-service/variables.env.bak
     cp configuration/reporting-ui/variables.env configuration/reporting-ui/variables.env.bak
 
-    docker run --rm --volumes-from postgres -v $(pwd)/backup:/var/backup "ubuntu" tar -czvf /var/backup/postgres.tar.gz /var/lib/postgresql/data
-    docker run --rm --volumes-from iam-db -v $(pwd)/backup:/var/backup "ubuntu" tar -czvf /var/backup/iam-db.tar.gz /var/lib/postgresql/data
-    docker run --rm --volumes-from elasticsearch -v $(pwd)/backup:/var/backup "ubuntu" tar -czvf /var/backup/elasticsearch.tar.gz /usr/share/elasticsearch/data
-    docker run --rm --volumes-from reporting-service -v $(pwd)/backup:/var/backup "ubuntu" tar -czvf /var/backup/reporting-service.tar.gz /opt/assets
+    docker run --rm --volumes-from postgres -v "$(pwd)"/backup:/var/backup "ubuntu" tar -czvf /var/backup/postgres.tar.gz /var/lib/postgresql/data
+    docker run --rm --volumes-from iam-db -v "$(pwd)"/backup:/var/backup "ubuntu" tar -czvf /var/backup/iam-db.tar.gz /var/lib/postgresql/data
+    docker run --rm --volumes-from elasticsearch -v "$(pwd)"/backup:/var/backup "ubuntu" tar -czvf /var/backup/elasticsearch.tar.gz /usr/share/elasticsearch/data
+    docker run --rm --volumes-from reporting-service -v "$(pwd)"/backup:/var/backup "ubuntu" tar -czvf /var/backup/reporting-service.tar.gz /opt/assets
   }
 
   restore() {
@@ -243,10 +243,10 @@
     cp configuration/reporting-service/variables.env.bak configuration/reporting-service/variables.env
     cp configuration/reporting-ui/variables.env.bak configuration/reporting-ui/variables.env
 
-    docker run --rm --volumes-from postgres -v $(pwd)/backup:/var/backup "ubuntu" bash -c "cd / && tar -xzvf /var/backup/postgres.tar.gz"
-    docker run --rm --volumes-from iam-db -v $(pwd)/backup:/var/backup "ubuntu" bash -c "cd / && tar -xzvf /var/backup/iam-db.tar.gz"
-    docker run --rm --volumes-from elasticsearch -v $(pwd)/backup:/var/backup "ubuntu" bash -c "cd / && tar -xzvf /var/backup/elasticsearch.tar.gz"
-    docker run --rm --volumes-from reporting-service -v $(pwd)/backup:/var/backup "ubuntu" bash -c "cd / && tar -xzvf /var/backup/reporting-service.tar.gz"
+    docker run --rm --volumes-from postgres -v "$(pwd)"/backup:/var/backup "ubuntu" bash -c "cd / && tar -xzvf /var/backup/postgres.tar.gz"
+    docker run --rm --volumes-from iam-db -v "$(pwd)"/backup:/var/backup "ubuntu" bash -c "cd / && tar -xzvf /var/backup/iam-db.tar.gz"
+    docker run --rm --volumes-from elasticsearch -v "$(pwd)"/backup:/var/backup "ubuntu" bash -c "cd / && tar -xzvf /var/backup/elasticsearch.tar.gz"
+    docker run --rm --volumes-from reporting-service -v "$(pwd)"/backup:/var/backup "ubuntu" bash -c "cd / && tar -xzvf /var/backup/reporting-service.tar.gz"
     down
   }
 
@@ -255,6 +255,7 @@
       exit 0
     fi
 
+    # shellcheck disable=SC1091
     source .env
     echo "reporting: ${TAG_REPORTING_SERVICE}"
   }
@@ -292,7 +293,7 @@
     #TODO: https://github.com/zebrunner/zebrunner/issues/328 organize debug logging for setup/replace
     file=$1
     #echo "file: $file"
-    content=$(<$file) # read the file's content into
+    content=$(<"$file") # read the file's content into
     #echo "content: $content"
 
     old=$2
@@ -303,13 +304,12 @@
     content=${content//"$old"/$new}
 
     #echo "content: $content"
-
-    printf '%s' "$content" >$file    # write new content to disk
+    printf '%s' "$content" >"$file"    # write new content to disk
   }
 
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-cd ${BASEDIR}
+cd ${BASEDIR} || exit
 
 case "$1" in
     setup)
